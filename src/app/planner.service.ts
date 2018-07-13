b6ae579ff5d377b4c885e745736ff5400e9a4653
JirkaPok <j.pok@post.cz>
2018-07-13 22:46:18 +0200

    fixed calculations still using only first gas

--- src/app/planner.service.ts
+++ src/app/planner.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@angular/core';
 export class Gas {
   constructor(public size: number, public startPressure: number) {
   }
+
+  public volume(): number {
+    return this.size * this.startPressure;
+  }
 }
 
 export class Diver {
@@ -38,11 +42,21 @@ export class Gases {
       }
     });
   }
+
+  public totalVolume(): number {
+    let total = 0;
+    this.current.forEach(gas => {
+      total += gas.volume();
+    });
+    return total;
+  }
 }
 
 export class Dive {
   public maxDepth: number;
   public maxTime: number;
+  public rockBottom: number;
+  public timeToSurface: number;
 }
 
 @Injectable()
@@ -56,7 +70,41 @@ export class PlannerService {
   }
 
   public calculate() {
-    this.dive.maxDepth = 300;
-    this.dive.maxTime = 50;
+    const averagePressure = this.averagePressure();
+    const timeToSurface = this.calculateTimeToSurface();
+    this.dive.timeToSurface = timeToSurface;
+    const rockBottom = this.calculateRockBottom(timeToSurface, averagePressure);
+    this.dive.rockBottom = rockBottom;
+    this.dive.maxDepth = this.calculateMaxDepth();
+    this.dive.maxTime = this.calculateMaxDiveTime(averagePressure, rockBottom);
+  }
+
+  private calculateMaxDiveTime(averagePressure: number, rockBottom: number): number {
+    const totalVolume = this.gases.totalVolume();
+    const availableGas = totalVolume - rockBottom * this.gases.current[0].size;
+    return availableGas / averagePressure / this.diver.sac;
+  }
+
+  private calculateRockBottom(timeToSurface: number, averagePressure: number): number {
+    const firstGas = this.gases.current[0];
+    const stressSac = 3 * this.diver.sac;
+    return timeToSurface * stressSac * averagePressure / firstGas.size;
+  }
+
+  private averagePressure(): number {
+    const averageDepth = this.plan.depth / 2;
+    return 1 + averageDepth / 10;
+  }
+
+  private calculateTimeToSurface(): number {
+    const solutionDuration = 2;
+    const swimSpeed = 10; // meter/min.
+    const safetyStop = this.dive.maxDepth >= 5 ? 3 : 0;
+    const swimTime = Math.ceil(this.dive.maxDepth / swimSpeed);
+    return solutionDuration + swimTime + safetyStop;
+  }
+
+  private calculateMaxDepth(): number {
+    return 30;
   }
 }

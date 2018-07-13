601abd3e1747eda2713a3a56d6f58ec6d40fbd38
JirkaPok <j.pok@post.cz>
2018-07-13 22:46:34 +0200

    added drop down for standard gases

--- src/app/gases/gases.component.ts
+++ src/app/gases/gases.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { PlannerService, Gases, Gas } from '../planner.service';
 
+export enum StandardGas {
+  Air = 21,
+  EAN32 = 32,
+  EAN36 = 36,
+  EAN38 = 38,
+  EAN50 = 50,
+  OXYGEN = 100
+}
+
 @Component({
   selector: 'app-gases',
   templateUrl: './gases.component.html',
@@ -8,11 +17,18 @@ import { PlannerService, Gases, Gas } from '../planner.service';
 })
 export class GasesComponent implements OnInit {
   public gases: Gases;
+  public gasNames: string[];
 
   constructor(private planer: PlannerService) { }
 
   ngOnInit() {
     this.gases = this.planer.gases;
+    this.gasNames = Object.keys(StandardGas)
+      .filter(k => typeof StandardGas[k] === 'number') as string[];
+  }
+
+  public assignStandardGas(gas: Gas, stgas: string): void {
+    gas.o2 = StandardGas[stgas];
   }
 
   public add(): void {

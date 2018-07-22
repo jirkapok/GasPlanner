f32040036700a8786d4cfa7a6a75e59fad905e50
JirkaPok <j.pok@post.cz>
2018-07-22 15:50:51 +0200

    Added test for sac calculator

--- src/app/shared/sac-calculator.service.spec.ts
+++ src/app/shared/sac-calculator.service.spec.ts
@@ -9,7 +9,32 @@ describe('SacCalculatorService', () => {
     });
   });
 
-  it('should be created', inject([SacCalculatorService], (service: SacCalculatorService) => {
-    expect(service).toBeTruthy();
+  it('15m for 45 min with 15L tank (defaults) has sac 20 L/min.', inject([SacCalculatorService], (service: SacCalculatorService) => {
+    expect(service.sac).toBe(20);
+  }));
+
+  it('15m for 60 min with 15L tank has sac 15 L/min.', inject([SacCalculatorService], (service: SacCalculatorService) => {
+    service.duration = 60;
+    expect(service.sac).toBe(15);
+  }));
+
+  it('at 0 m calculates 50 L/min.', inject([SacCalculatorService], (service: SacCalculatorService) => {
+    service.depth = 0;
+    expect(service.sac).toBe(50);
+  }));
+
+  it('0 bar consumed has SAC 0 L/min.', inject([SacCalculatorService], (service: SacCalculatorService) => {
+    service.used = 0;
+    expect(service.sac).toBe(0);
+  }));
+
+  it('0 L large tank has SAC 0 L/min.', inject([SacCalculatorService], (service: SacCalculatorService) => {
+    service.tank = 0;
+    expect(service.sac).toBe(0);
+  }));
+
+  it('for 0 min has infinite SAC', inject([SacCalculatorService], (service: SacCalculatorService) => {
+    service.duration = 0;
+    expect(service.sac).toBe(Infinity);
   }));
 });

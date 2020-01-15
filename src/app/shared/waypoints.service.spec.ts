731d0c108fca4124a1ff4b7450552082e2bf8637
JirkaPok <j.pok@post.cz>
2020-01-15 23:47:55 +0100

    Added test for shallow water waypoints calculation

--- src/app/shared/waypoints.service.spec.ts
+++ src/app/shared/waypoints.service.spec.ts
@@ -3,17 +3,24 @@ import { TestBed, inject } from '@angular/core/testing';
 import { WayPointsService } from './waypoints.service';
 import { Plan, Strategies } from './models';
 
-describe('WayPointsService', () => {
+fdescribe('WayPointsService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [WayPointsService]
     });
   });
 
-  it('30m for 20 min calculates all waypoints', inject([WayPointsService], (service: WayPointsService) => {
-    const plan = new Plan(20, 30, Strategies.ALL);
+  it('30m for 20 min calculates all way points', inject([WayPointsService], (service: WayPointsService) => {
+    const plan = new Plan(30, 20, Strategies.ALL);
 
     const wayPoints = WayPointsService.calculateWayPoints(plan);
     expect(wayPoints.length).toBe(5);
+  }));
+
+  it('10m for 30 min calculates all way points', inject([WayPointsService], (service: WayPointsService) => {
+    const plan = new Plan(30, 10, Strategies.ALL);
+
+    const wayPoints = WayPointsService.calculateWayPoints(plan);
+    expect(wayPoints.length).toBe(3);
   }));
 });

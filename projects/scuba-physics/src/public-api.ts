a6786e762b18634743efcc62fba003596cdb6c7c
JirkaPok <j.pok@post.cz>
2020-01-22 00:25:40 +0100

    Cleanup in physics lib

--- projects/scuba-physics/src/public-api.ts
+++ projects/scuba-physics/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of scuba-physics
  */
 
-export * from './lib/scuba-physics.service';
-export * from './lib/scuba-physics.component';
-export * from './lib/scuba-physics.module';
+export class TestClass {}

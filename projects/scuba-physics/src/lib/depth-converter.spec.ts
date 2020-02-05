d3403354a7d287b79821afec6eaaa9251849dde2
JirkaPok <j.pok@post.cz>
2020-02-05 15:56:24 -0800

    Added test for salth water pressure conversions

--- projects/scuba-physics/src/lib/depth-converter.spec.ts
+++ projects/scuba-physics/src/lib/depth-converter.spec.ts
@@ -12,4 +12,16 @@ describe('Depth Converter', () => {
       expect(result).toBeCloseTo(22.43, 2);
     });
   });
+
+  describe('Salst water', () => {
+    it('22m converts to 3.222 bar', () => {
+      const result = DepthConverter.toBar(22, false);
+      expect(result).toBeCloseTo(3.222, 3);
+    });
+
+    it('3.2 converts to 22.43 m', () => {
+      const result = DepthConverter.fromBar(3.2, false);
+      expect(result).toBeCloseTo(21.780, 3);
+    });
+  });
 });

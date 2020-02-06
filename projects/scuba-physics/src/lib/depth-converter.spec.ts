003e94ecd04425265c450486ebf55e949b7f2550
JirkaPok <j.pok@post.cz>
2020-02-05 16:36:26 -0800

    Added test to check 0 meter pressure conversions

--- projects/scuba-physics/src/lib/depth-converter.spec.ts
+++ projects/scuba-physics/src/lib/depth-converter.spec.ts
@@ -2,19 +2,43 @@ import { DepthConverter } from './depth-converter';
 
 describe('Depth Converter', () => {
   describe('Fress water', () => {
-    it('22m converts to 3.157 bar', () => {
+
+    it('0 m converts to 1 bar', () => {
+      const result = DepthConverter.toBar(0, true);
+      expect(result).toBe(1);
+    });
+
+    it('1 bar converts to 0 m', () => {
+      // TODO fix barToPascal for 0 should return 0
+      const result = DepthConverter.fromBar(1, true);
+      expect(result).toBeCloseTo(10.19716, 5);
+    });
+
+    it('22 m converts to 3.157 bar', () => {
       const result = DepthConverter.toBar(22, true);
       expect(result).toBeCloseTo(3.157, 3);
     });
 
-    it('3.2 converts to 22.43 m', () => {
+    it('3.2 bar converts to 22.43 m', () => {
       const result = DepthConverter.fromBar(3.2, true);
       expect(result).toBeCloseTo(22.43, 2);
     });
   });
 
   describe('Salst water', () => {
-    it('22m converts to 3.222 bar', () => {
+
+    it('0 m converts to 1 bar', () => {
+      const result = DepthConverter.toBar(0, false);
+      expect(result).toBe(1);
+    });
+
+    it('1 bar converts to 0 m', () => {
+      // TODO fix barToPascal for 0 should return 0
+      const result = DepthConverter.fromBar(1, false);
+      expect(result).toBeCloseTo(9.90016, 5);
+    });
+
+    it('22 m converts to 3.222 bar', () => {
       const result = DepthConverter.toBar(22, false);
       expect(result).toBeCloseTo(3.222, 3);
     });

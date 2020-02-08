99a32357d2c09bb4d11b5068ccb04ddc85be2625
JirkaPok <j.pok@post.cz>
2020-02-08 11:32:50 -0800

    Changed grouping of pressure related tests

--- projects/scuba-physics/src/lib/pressure-converter.spec.ts
+++ projects/scuba-physics/src/lib/pressure-converter.spec.ts
@@ -1,75 +1,75 @@
 import { PressureConverter, VapourPressure, Gravity, AltitudePressure } from './pressure-converter';
 
-describe('Pressure Converter', () => {
-  it('315700 pascals converts to 3.157 bar', () => {
-    const result = PressureConverter.pascalToBar(315700);
-    expect(result).toBeCloseTo(3.157, 3);
-  });
-
-  it('3.157 converts to 315700 pascals', () => {
-    const result = PressureConverter.barToPascal(3.157);
-    expect(result).toBeCloseTo(315700);
-  });
-
-  describe('Partial pressure', () => {
-    it('At 1 bar 0.79 volume fraction converts to ', () => {
-      const result = PressureConverter.partialPressure(1, 0.79);
-      expect(result).toBe(0.79);
+describe('Pressure', () => {
+  describe('Pressure Converter', () => {
+    it('315700 pascals converts to 3.157 bar', () => {
+      const result = PressureConverter.pascalToBar(315700);
+      expect(result).toBeCloseTo(3.157, 3);
     });
 
-    it('At 3 bar 0.79 volume fraction converts to ', () => {
-      const result = PressureConverter.partialPressure(6.667, 0.21);
-      expect(result).toBeCloseTo(1.40, 3);
-    });
-  
-    it('At 0 bars any fraction results in 0 partial pressure', () => {
-      const result = PressureConverter.partialPressure(0, 0.79);
-      expect(result).toBe(0);
+    it('3.157 converts to 315700 pascals', () => {
+      const result = PressureConverter.barToPascal(3.157);
+      expect(result).toBeCloseTo(315700);
     });
 
-    it('At any bars 0 fraction results in 0 partial pressure', () => {
-      const result = PressureConverter.partialPressure(3, 0);
-      expect(result).toBe(0);
+    describe('Partial pressure', () => {
+      it('At 1 bar 0.79 volume fraction converts to ', () => {
+        const result = PressureConverter.partialPressure(1, 0.79);
+        expect(result).toBe(0.79);
+      });
+
+      it('At 3 bar 0.79 volume fraction converts to ', () => {
+        const result = PressureConverter.partialPressure(6.667, 0.21);
+        expect(result).toBeCloseTo(1.40, 3);
+      });
+
+      it('At 0 bars any fraction results in 0 partial pressure', () => {
+        const result = PressureConverter.partialPressure(0, 0.79);
+        expect(result).toBe(0);
+      });
+
+      it('At any bars 0 fraction results in 0 partial pressure', () => {
+        const result = PressureConverter.partialPressure(3, 0);
+        expect(result).toBe(0);
+      });
     });
   });
-});
 
-describe('Vapour pressure', () => {
-  it('0°C results in out of range', () => {
-    expect(() => VapourPressure.waterVapourPressureInBars(0)).toThrow();
+  describe('Vapour pressure', () => {
+    it('0°C results in out of range', () => {
+      expect(() => VapourPressure.waterVapourPressureInBars(0)).toThrow();
+    });
+
+    it('1°C corresponds to 0.00651 bar', () => {
+      const result = VapourPressure.waterVapourPressureInBars(1);
+      expect(result).toBeCloseTo(0.00651, 5);
+    });
+
+    it('35.2°C corresponds to 0.056714 bar', () => {
+      const result = VapourPressure.waterVapourPressureInBars(35.2);
+      expect(result).toBeCloseTo(0.056714, 5);
+    });
+
+    it('99°C corresponds to 0.97758 bar', () => {
+      const result = VapourPressure.waterVapourPressureInBars(99);
+      expect(result).toBeCloseTo(0.97758, 5);
+    });
+
+    it('100°C corresponds to 1.01334 bar', () => {
+      const result = VapourPressure.waterVapourPressureInBars(100);
+      expect(result).toBeCloseTo(1.013365, 5);
+    });
+
+    it('374°C corresponds to 217.3 bar', () => {
+      const result = VapourPressure.waterVapourPressureInBars(374);
+      expect(result).toBeCloseTo(217.30381, 5);
+    });
+
+    it('376°C corresponds to out of range', () => {
+      expect(() => VapourPressure.waterVapourPressureInBars(376)).toThrow();
+    });
   });
 
-  it('1°C corresponds to 0.00651 bar', () => {
-    const result = VapourPressure.waterVapourPressureInBars(1);
-    expect(result).toBeCloseTo(0.00651, 5);
-  });
- 
-  it('35.2°C corresponds to 0.056714 bar', () => {
-    const result = VapourPressure.waterVapourPressureInBars(35.2);
-    expect(result).toBeCloseTo(0.056714, 5);
-  });
-  
-  it('99°C corresponds to 0.97758 bar', () => {
-    const result = VapourPressure.waterVapourPressureInBars(99);
-    expect(result).toBeCloseTo(0.97758, 5);
-  });
-
-  it('100°C corresponds to 1.01334 bar', () => {
-    const result = VapourPressure.waterVapourPressureInBars(100);
-    expect(result).toBeCloseTo(1.013365, 5);
-  });
-
-  it('374°C corresponds to 217.3 bar', () => {
-    const result = VapourPressure.waterVapourPressureInBars(374);
-    expect(result).toBeCloseTo(217.30381, 5);
-  });
-
-  it('376°C corresponds to out of range', () => {
-    expect(() => VapourPressure.waterVapourPressureInBars(376)).toThrow();
-  });
-});
-
-describe('Altitude pressure', () => {
   describe('Gravity', () => {
     it('At sea level is 9.807', () => {
       const gravity = Gravity.atAltitude(0);
@@ -86,31 +86,31 @@ describe('Altitude pressure', () => {
       expect(gravity).toBeCloseTo(9.79742, 5);
     });
   });
-});
 
-describe('Altitude pressure', () => {
-  it('At sea level is 10325 Pa', () => {
-    const gravity = AltitudePressure.atAltitude(0);
-    expect(gravity).toBeCloseTo(101325);
-  });
+  describe('Altitude pressure', () => {
+    it('At sea level is 10325 Pa', () => {
+      const gravity = AltitudePressure.atAltitude(0);
+      expect(gravity).toBeCloseTo(101325);
+    });
 
-  it('400 level is 96611 Pa', () => {
-    const gravity = AltitudePressure.atAltitude(400);
-    expect(gravity).toBeCloseTo(96611, 0);
-  });
+    it('400 level is 96611 Pa', () => {
+      const gravity = AltitudePressure.atAltitude(400);
+      expect(gravity).toBeCloseTo(96611, 0);
+    });
 
-  it('1000 level is 89875 Pa', () => {
-    const gravity = AltitudePressure.atAltitude(1000);
-    expect(gravity).toBeCloseTo(89875, 0);
-  });
+    it('1000 level is 89875 Pa', () => {
+      const gravity = AltitudePressure.atAltitude(1000);
+      expect(gravity).toBeCloseTo(89875, 0);
+    });
 
-  it('2000 level is 79495 Pa', () => {
-    const gravity = AltitudePressure.atAltitude(2000);
-    expect(gravity).toBeCloseTo(79495, 0);
-  });
+    it('2000 level is 79495 Pa', () => {
+      const gravity = AltitudePressure.atAltitude(2000);
+      expect(gravity).toBeCloseTo(79495, 0);
+    });
 
-  it('3000 level is 70109 Pa', () => {
-    const gravity = AltitudePressure.atAltitude(3000);
-    expect(gravity).toBeCloseTo(70109, 0);
+    it('3000 level is 70109 Pa', () => {
+      const gravity = AltitudePressure.atAltitude(3000);
+      expect(gravity).toBeCloseTo(70109, 0);
+    });
   });
 });

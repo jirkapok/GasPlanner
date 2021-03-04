ab2624ab742b24c6f03cf148975d92ea03d4cc53
JirkaPok <j.pok@post.cz>
2021-03-04 13:59:45 -0800

    Changed user settings icon

--- projects/planner/src/app/diver/diver.component.ts
+++ projects/planner/src/app/diver/diver.component.ts
@@ -1,28 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { Router } from '@angular/router';
+import { faUserCog } from '@fortawesome/free-solid-svg-icons';
 
 import { PlannerService } from '../shared/planner.service';
 import { Diver } from 'scuba-physics';
 
 @Component({
-  selector: 'app-diver',
-  templateUrl: './diver.component.html',
-  styleUrls: ['./diver.component.css']
+    selector: 'app-diver',
+    templateUrl: './diver.component.html',
+    styleUrls: ['./diver.component.css']
 })
 export class DiverComponent implements OnInit {
-  public diver: Diver;
-  public user = faUser;
-  
-  constructor(private planer: PlannerService, private router: Router) {
-    this.diver = this.planer.diver;
-  }
+    public diver: Diver;
+    public icon = faUserCog;
 
-  public goBack(): void {
-    this.router.navigateByUrl('/');
-  }
+    constructor(private planer: PlannerService, private router: Router) {
+        this.diver = this.planer.diver;
+    }
 
-  ngOnInit() {
+    public goBack(): void {
+        this.router.navigateByUrl('/');
+    }
 
-  }
+    ngOnInit() {
+
+    }
 }

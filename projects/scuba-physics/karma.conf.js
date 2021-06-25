ce589b4467fc1e0212730eff2bccc64907aa0041
JirkaPok <j.pok@post.cz>
2021-06-25 06:37:54 -0700

    Restored tests results for git actions

--- projects/scuba-physics/karma.conf.js
+++ projects/scuba-physics/karma.conf.js
@@ -9,6 +9,7 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
+      require('karma-junit-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
@@ -32,7 +33,11 @@ module.exports = function (config) {
         { type: 'text-summary' }
       ]
     },
-    reporters: ['progress', 'kjhtml'],
+    junitReporter: {
+        outputDir: '../../test-results/scuba-physic', // results will be saved as $outputDir/$browserName.xml
+        suite: 'scuba-physics'
+    },
+    reporters: ['progress', 'kjhtml', 'junit'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,

14909de7f41a983e44e4bc58bf8a1a8b5d46d6a7
JirkaPok <j.pok@post.cz>
2022-05-30 08:17:17 -0700

    Fixed es-lint rules

--- projects/planner/src/app/shared/nitrox-calculator.service.ts
+++ projects/planner/src/app/shared/nitrox-calculator.service.ts
@@ -15,8 +15,7 @@ export class NitroxCalculatorService {
     private _fO2 = 50;
     private _mod = 22;
     private _calculation = NitroxMode.Mod;
-    private calculate: () => void = this.calculateCurrentMod;
-    // consider usage of switch in the UI to change the depth converter
+
     private nitroxCalculator: NitroxCalculator;
 
     constructor() {
@@ -32,37 +31,12 @@ export class NitroxCalculatorService {
         return this._calculation;
     }
 
-    public set calculation(newValue: NitroxMode) {
-        this._calculation = newValue;
-
-        switch (newValue) {
-            case NitroxMode.BestMix:
-                this.calculate = this.calculateBestMix;
-                break;
-            case NitroxMode.PO2:
-                this.calculate = this.calculatePartial;
-                break;
-            default:
-                this.calculate = this.calculateCurrentMod;
-        }
-    }
-
     public get pO2(): number {
         return this._pO2;
     }
 
-    public set pO2(newValue: number) {
-        this._pO2 = newValue;
-        this.calculate();
-    }
-
     public get fO2(): number {
         return this._fO2;
-    }
-
-    public set fO2(newValue: number) {
-        this._fO2 = newValue;
-        this.calculate();
     }
 
     public get ead(): number {
@@ -73,20 +47,47 @@ export class NitroxCalculatorService {
         return this._mod;
     }
 
+    public set calculation(newValue: NitroxMode) {
+        this._calculation = newValue;
+
+        switch (newValue) {
+            case NitroxMode.BestMix:
+                this.calculate = () => this.calculateBestMix;
+                break;
+            case NitroxMode.PO2:
+                this.calculate = () => this.calculatePartial;
+                break;
+            default:
+                this.calculate = () => this.calculateCurrentMod;
+        }
+    }
+
+    public set pO2(newValue: number) {
+        this._pO2 = newValue;
+        this.calculate();
+    }
+
+    public set fO2(newValue: number) {
+        this._fO2 = newValue;
+        this.calculate();
+    }
+
     public set mod(newValue: number) {
         this._mod = newValue;
         this.calculate();
     }
 
-    private calculateCurrentMod() {
+    private calculateCurrentMod = () => {
         this._mod = this.nitroxCalculator.mod(this._pO2, this._fO2);
-    }
+    };
 
-    private calculateBestMix() {
+    private calculateBestMix = () => {
         this._fO2 = this.nitroxCalculator.bestMix(this._pO2, this._mod);
-    }
+    };
 
-    private calculatePartial() {
+    private calculatePartial = () => {
         this._pO2 = this.nitroxCalculator.partialPressure(this._fO2, this._mod);
-    }
+    };
+
+    private calculate = () => this.calculateCurrentMod;
 }

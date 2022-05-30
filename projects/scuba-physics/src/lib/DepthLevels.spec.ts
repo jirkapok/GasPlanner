afc09cc1ad77dba262425646af1ea33456441777
JirkaPok <j.pok@post.cz>
2022-05-30 16:08:57 -0700

    Added depth level tests to cover minimum auto stop and stops distance

--- projects/scuba-physics/src/lib/DepthLevels.spec.ts
+++ projects/scuba-physics/src/lib/DepthLevels.spec.ts
@@ -4,67 +4,93 @@ import { SafetyStop } from './Options';
 
 describe('Depth Levels', () => {
     const irrelevantDepthConverter = DepthConverter.simple();
+    const options = {
+        lastStopDepth: 5,
+        safetyStop: SafetyStop.always,
+        decoStopDistance: 3,
+        minimumAutoStopDepth: 10
+    };
+
+    const levels = new DepthLevels(irrelevantDepthConverter, options);
+
+    beforeEach(() => {
+        options.safetyStop = SafetyStop.always;
+        options.decoStopDistance = 3;
+        options.minimumAutoStopDepth = 10;
+    });
 
     describe('Next stop', () => {
-        const options = {
-            lastStopDepth: 5,
-            safetyStop: SafetyStop.always,
-            decoStopDistance: 3,
-            minimumAutoStopDepth: 10
-        };
-
         it('Rounds to 3 meter increments', () => {
             options.lastStopDepth = 3;
-            const levels = new DepthLevels(irrelevantDepthConverter, options);
             const result = levels.nextStop(17.6);
             expect(result).toBe(15);
         });
 
         it('At 12 m returns first stop at 9 meters', () => {
             options.lastStopDepth = 3;
-            const levels = new DepthLevels(irrelevantDepthConverter, options);
             const result = levels.nextStop(12);
             expect(result).toBe(9);
         });
 
         it('At 12 m returns next stop at 9 meters', () => {
             options.lastStopDepth = 3;
-            const levels = new DepthLevels(irrelevantDepthConverter, options);
             const result = levels.nextStop(9);
             expect(result).toBe(6);
         });
 
         it('Returns 0 m bellow 3 m last stop', () => {
             options.lastStopDepth = 6;
-            const levels = new DepthLevels(irrelevantDepthConverter, options);
             const result = levels.nextStop(5);
             expect(result).toBe(0);
         });
 
         it('Is 5 m from 6 m in case last stop 5 m', () => {
             options.lastStopDepth = 5;
-            const levels = new DepthLevels(irrelevantDepthConverter, options);
             const result = levels.nextStop(6);
             expect(result).toBe(5);
         });
 
         it('Is 0 meters at 6 meters in case last stop 6 m', () => {
             options.lastStopDepth = 6;
-            const levels = new DepthLevels(irrelevantDepthConverter, options);
             const result = levels.nextStop(6);
             expect(result).toBe(0);
         });
+
+        describe('With 5 m Deco stop distance', () => {
+            beforeEach(() => {
+                options.decoStopDistance = 5;
+            });
+
+            it('Is 20 meters at 23 meters', () => {
+                const result = levels.nextStop(23);
+                expect(result).toBe(20);
+            });
+
+            it('Is 15 meters at 20 meters', () => {
+                const result = levels.nextStop(20);
+                expect(result).toBe(15);
+            });
+        });
     });
 
+
+
     describe('Add safety stop', () => {
+        it('Minimum auto stop depth is applied', () => {
+            options.minimumAutoStopDepth = 20;
+            options.safetyStop = SafetyStop.auto;
+            const result = levels.addSafetyStop(5, 15);
+            expect(result).toBeFalsy();
+        });
+
+        beforeEach(() => {
+            options.minimumAutoStopDepth = 10;
+        });
+
         describe('Always', () => {
-            const options = {
-                lastStopDepth: 5,
-                safetyStop: SafetyStop.always,
-                decoStopDistance: 3,
-                minimumAutoStopDepth: 10
-            };
-            const levels = new DepthLevels(irrelevantDepthConverter, options);
+            beforeEach(() => {
+                options.safetyStop = SafetyStop.always;
+            });
 
             it('Deep dive below last stop', () => {
                 const result = levels.addSafetyStop(7, 40);
@@ -88,13 +114,9 @@ describe('Depth Levels', () => {
         });
 
         describe('Auto', () => {
-            const options = {
-                lastStopDepth: 5,
-                safetyStop: SafetyStop.auto,
-                decoStopDistance: 3,
-                minimumAutoStopDepth: 10
-            };
-            const levels = new DepthLevels(irrelevantDepthConverter, options);
+            beforeEach(() => {
+                options.safetyStop = SafetyStop.auto;
+            });
 
             it('Deep dive below stop', () => {
                 const result = levels.addSafetyStop(10, 40);
@@ -118,13 +140,9 @@ describe('Depth Levels', () => {
         });
 
         describe('Never', () => {
-            const options = {
-                lastStopDepth: 5,
-                safetyStop: SafetyStop.never,
-                decoStopDistance: 3,
-                minimumAutoStopDepth: 10
-            };
-            const levels = new DepthLevels(irrelevantDepthConverter, options);
+            beforeEach(() => {
+                options.safetyStop = SafetyStop.never;
+            });
 
             it('Deep dive, below stop', () => {
                 const result = levels.addSafetyStop(7, 40);

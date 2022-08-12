3c2cb0d051aa6ced9640d70014d224a98f6a0748
JirkaPok <j.pok@post.cz>
2022-08-12 14:43:46 -0700

    Fixed toxicity usage in tanks and ndl limits

---
+++ projects/planner/src/app/ndl-limits/ndl-limits.component.spec.ts
@@ -0,0 +1,47 @@
+import { ComponentFixture, TestBed, inject } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NdlService } from '../shared/ndl.service';
+import { UnitConversion } from '../shared/UnitConversion';
+import { NdlLimitsComponent } from './ndl-limits.component';
+
+describe('NdlLimits component', () => {
+    let component: NdlLimitsComponent;
+    let fixture: ComponentFixture<NdlLimitsComponent>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [NdlLimitsComponent],
+            providers: [FormsModule, UnitConversion, NdlService],
+            imports: [RouterTestingModule.withRoutes([])]
+        })
+            .compileComponents();
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(NdlLimitsComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    xit('Imperial units adjusts calculated depths to feet', inject([UnitConversion],
+        (units: UnitConversion) => {
+            units.imperialUnits = true;
+            component.calculate();
+            // TODO imperial units
+            expect(component.limits[0].depth).toBe(40);
+        }));
+
+    it('Change of max ppO2 causes mod for limits', () => {
+        component.options.maxPpO2 = 1;
+        component.calculate();
+        const lastIndex = component.limits.length - 1;
+        expect(component.limits[lastIndex].depth).toBe(36);
+    });
+
+    it('No limits found', () => {
+        component.tank.o2 = 100;
+        component.calculate();
+        expect(component.noResults).toBeTruthy();
+    });
+});
--- projects/planner/src/app/ndl-limits/ndl-limits.component.ts
+++ projects/planner/src/app/ndl-limits/ndl-limits.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { faTable, faCog } from '@fortawesome/free-solid-svg-icons';
 import { Options, Tank, Time } from 'scuba-physics';
+import { GasToxicity } from '../shared/gasToxicity.service';
 import { NdlLimit, NdlService } from '../shared/ndl.service';
 import { UnitConversion } from '../shared/UnitConversion';
 import { TankBound } from '../tanks/tanks.component';
@@ -16,13 +17,16 @@ export class NdlLimitsComponent {
     public icon = faTable;
     public iconConfig = faCog;
     public tank: TankBound;
-    public options = new Options();
+    public options: Options;
     public totalDuration = Time.oneDay;
     public isComplex = false;
     public limits: NdlLimit[] = [];
+    public toxicity: GasToxicity;
 
     constructor(private router: Router, public units: UnitConversion, private ndl: NdlService) {
         this.tank = new TankBound(new Tank(15, 200, 21), this.units);
+        this.options = new Options();
+        this.toxicity = new GasToxicity(this.options);
         this.calculate();
     }
 
@@ -31,10 +35,10 @@ export class NdlLimitsComponent {
     }
 
     public calculate(): void {
+        // TODO depths return need to be converted to defined units
         this.limits = this.ndl.calculate(this.tank.tank.gas, this.options);
     }
 
-    // TODO Nitrox component uses planner to get its values to label, the same applies to tank
     public async goBack(): Promise<boolean> {
         return await this.router.navigateByUrl('/');
     }

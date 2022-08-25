3a36bb8c393f94504fc530fb33f07a4b9ea4a750
JirkaPok <j.pok@post.cz>
2022-08-25 14:30:16 -0700

    Added settings for VS code extensions

--- installEnvironment.ps1
+++ installEnvironment.ps1
@@ -4,13 +4,17 @@
 choco install nvm -y
 nvm install -lts
 nvm use -lts
+npm install -g @angular/cli
+npm install -g typescript
 
 # Visual Studio Code
 choco install vscode -y
-# Optional
+# Optional extensions
 # code --install-extension ms-vscode.powershell
-npm install -g @angular/cli
-npm install -g typescript
+# code --install-extension angular.ng-template
+# code --install-extension hbenl.vscode-test-explorer
+# code --install-extension lucono.karma-test-explorer
+# code --install-extension hbenl.vscode-jasmine-test-adapter
 
 # NPM Packages
 .\build.ps1

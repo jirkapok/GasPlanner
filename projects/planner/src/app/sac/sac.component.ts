8e63bd934b2f4df5faf673ac22e221398f6e11db
JirkaPok <j.pok@post.cz>
2022-08-26 11:36:46 -0700

    Reduced usage of calc in sac component

--- projects/planner/src/app/sac/sac.component.ts
+++ projects/planner/src/app/sac/sac.component.ts
@@ -16,8 +16,8 @@ export class SacComponent {
     public calcIcon = faCalculator;
 
     constructor(
-        private router: Router,
-        private planer: PlannerService, public calc: SacCalculatorService, public units: UnitConversion) {
+        private router: Router, private planer: PlannerService,
+        private calc: SacCalculatorService, public units: UnitConversion) {
     }
 
     public get ranges(): RangeConstants {
@@ -52,6 +52,10 @@ export class SacComponent {
         return this.units.fromLiter(this.calc.rmv);
     }
 
+    public get calcDuration(): number {
+        return this.calc.duration;
+    }
+
     public set calcDepth(newValue: number) {
         this.calc.depth = this.units.toMeters(newValue);
     }
@@ -66,6 +70,10 @@ export class SacComponent {
 
     public set calcRmv(newValue: number) {
         this.calc.rmv = this.units.toLiter(newValue);
+    }
+
+    public set calcDuration(newValue: number) {
+        this.calc.duration = newValue;
     }
 
     public toDuration(): void {

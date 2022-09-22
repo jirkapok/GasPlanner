82b92ec81af039cd2acb81f6becb1dcb0c443d4c
JirkaPok <j.pok@post.cz>
2022-09-21 18:47:59 -0700

    Simplified OTU implementation

--- projects/scuba-physics/src/lib/OtuCalculator.spec.ts
+++ projects/scuba-physics/src/lib/OtuCalculator.spec.ts
@@ -23,13 +23,13 @@ describe('OtuCalculatorService', () => {
 
         it('OTU counts as sum of all segments', () => {
             const otu = otuCalculator.calculateForProfile(profile);
-            expect(otu).toBe(64.7108642683392);
+            expect(otu).toBeCloseTo(64.8711617, 7);
         });
 
         it('Applies depth converter', () => {
             const calculator = new OtuCalculator(DepthConverter.forSaltWater());
             const otu = calculator.calculateForProfile(profile);
-            expect(otu).toBe(65.75466537123029);
+            expect(otu).toBeCloseTo(65.9177262, 7);
         });
     });
 
@@ -50,25 +50,24 @@ describe('OtuCalculatorService', () => {
         });
 
         it('38.6762 OTU for 22min at 1.484 ppO2', () => {
-            // values obtained from Bakers paper
-            const otu = otuCalculator.calculate(22.0, 1.484, 0, 0);
+            const otu = otuCalculator.calculate(1320, 1.484, 0, 0);
             expect(otu).toBe(38.676181761978775);
         });
 
         it('38.5818 OTU for 20 min at 1.6 ppO2', () => {
-            const otu = otuCalculator.calculate(20.0, 1.6, 0, 0);
-            expect(otu).toBe(38.58177728187526);
+            const otu = otuCalculator.calculate(1200, 1.6, 0, 0);
+            expect(otu).toBeCloseTo(38.5817773, 7);
         });
 
         it('28.9712 OTU for 20 min with EAN32 in 30 metres', () => {
-            const otu = otuCalculator.calculate(20, .32, 30, 30);
-            expect(otu).toBe(28.971245142600388);
+            const otu = otuCalculator.calculate(1200, .32, 30, 36);
+            expect(otu).toBeCloseTo(31.9046752, 7);
         });
     });
 
     describe('Difference - depth does change', () => {
         it('0 OTU for 20 min with 0 pO2 in 30 metres', () => {
-            const otu = otuCalculator.calculate(20, 0, 20, 30);
+            const otu = otuCalculator.calculate(1200, 0, 20, 30);
             expect(otu).toBe(0);
         });
 
@@ -78,18 +77,18 @@ describe('OtuCalculatorService', () => {
         });
 
         it('28.9 OTU for 20 min with EAN32 in 30 metres', () => {
-            const otu = otuCalculator.calculate(22, .32, 36, 36);
+            const otu = otuCalculator.calculate(1320, .32, 36, 36);
             expect(otu).toBe(38.28272978563932);
         });
 
         it('2.4026 OTU for 3 min with EAN32 from 0 m to 36 m', () => {
-            const otu = otuCalculator.calculate(3, .32, 0, 36);
-            expect(otu).toBe(2.4025576802454425);
+            const otu = otuCalculator.calculate(180, .32, 0, 36);
+            expect(otu).toBeCloseTo(2.4171302, 7);
         });
 
         it('24.0256 OTU for 30 min with EAN32 from 36 m to 0 m', () => {
-            const otu = otuCalculator.calculate(30, .32, 36, 0);
-            expect(otu).toBe(24.02557680245443);
+            const otu = otuCalculator.calculate(1800, .32, 36, 0);
+            expect(otu).toBeCloseTo(24.1713018, 7);
         });
     });
 });
--- projects/scuba-physics/src/lib/OtuCalculator.ts
+++ projects/scuba-physics/src/lib/OtuCalculator.ts
@@ -2,32 +2,26 @@ import { DepthConverter } from './depth-converter';
 import { Segment } from './Segments';
 import { Time } from './Time';
 
-type OTU = number;
-
 /**
  * OTU - Oxygen Toxicity Units
  *
  * Oxygen Toxicity Calculations
  * Reference: https://www.shearwater.com/wp-content/uploads/2012/08/Oxygen_Toxicity_Calculations.pdf
  *
- * @author Michael Czolko <michael@czolko.cz>
  */
 export class OtuCalculator {
     public static readonly dailyLimit = 300;
-    private static readonly minPressure = 0.5;
-    private static readonly threeElevenths = 3 / 11;
-    private static readonly elevenSixths = 11 / 6;
+    private readonly minPressure = 0.5;
 
-    constructor(private depthConverter: DepthConverter) {}
+    constructor(private depthConverter: DepthConverter) { }
 
     /** Calculates total OTU units for provided profile */
-    public calculateForProfile(profile: Segment[]): OTU {
+    public calculateForProfile(profile: Segment[]): number {
         let total = 0;
 
         profile.forEach(segment => {
             const o2 = segment.gas.fO2;
-            const minutes = Time.toMinutes(segment.duration);
-            const partOtu = this.calculate(minutes, o2, segment.startDepth, segment.endDepth);
+            const partOtu = this.calculate(segment.duration, o2, segment.startDepth, segment.endDepth);
             total += partOtu;
         });
 
@@ -38,69 +32,35 @@ export class OtuCalculator {
      * Ascent or descent profile at a constant rate
      * AAP - Absolute Atmospheric Pressure
      *
-     * @param time - time in minutes
+     * @param duration - time in seconds
      * @param pO2 - partial oxygen concentration (EAN32 = 0.32)
      * @param startDepth - start depth in meters
      * @param endDepth - end depth in meters
      */
-    public calculate(time: number, pO2: number, startDepth: number, endDepth: number): OTU {
-        // consider merge into one formula as described:
-        // https://thetheoreticaldiver.org/wordpress/index.php/2018/12/05/a-few-thoughts-on-oxygen-toxicity/
-        if (startDepth === endDepth) {
-            return this.calculateFlatWithDepth(time, pO2, startDepth);
-        }
-
-        return this.calculateDifference(time, pO2, startDepth, endDepth);
-    }
-
-    /**
-     * only for start and end depths are equal
-     */
-    private calculateFlatWithDepth(time: number, pO2: number, depth: number): OTU {
-        const pressure = this.depthConverter.toBar(depth);
-        const ppO2 = pO2 * pressure;
-        return this.calculateFlat(time, ppO2);
-    }
-
-    private calculateFlat(time: number, ppO2: number): OTU {
-        if (ppO2 < OtuCalculator.minPressure) {
-            return 0;
-        }
-
-        return time * (Math.pow((.5 / (ppO2 - .5)), -5 / 6));
-    }
-
-    /**
-     * Only for different start and end depths
-     * (Ascent or descent profile at a constant rate)
-     */
-    private calculateDifference(time: number, pO2: number, startDepth: number, endDepth: number): OTU {
-        // AAP - Absolute Atmospheric Pressure
+    public calculate(duration: number, pO2: number, startDepth: number, endDepth: number): number {
+        let durationMinutes = Time.toMinutes(duration);
         const startAAP = this.depthConverter.toBar(startDepth);
         const endAAP = this.depthConverter.toBar(endDepth);
-        const maxAAP = Math.max(startAAP, endAAP);
-        const minAAP = Math.min(startAAP, endAAP);
-        const maxPO2 = maxAAP * pO2;
-        const minP02 = minAAP * pO2;
+        let pO2Start = startAAP * pO2;
+        let pO2End = endAAP * pO2;
 
-        if (maxPO2 <= OtuCalculator.minPressure) {
+        if ((pO2Start <= this.minPressure) && (pO2End <= this.minPressure)) {
             return 0;
         }
 
-        const lowPO2 = minP02 < OtuCalculator.minPressure ? OtuCalculator.minPressure : minP02;
-        const oxygenTime = time * (maxPO2 - lowPO2) / (maxPO2 - minP02);
-        return this.differenceEquation(oxygenTime, maxPO2, lowPO2);
-    }
+        // only part of the segment bellow limit
+        if (pO2Start <= this.minPressure) {
+            durationMinutes = durationMinutes * (pO2End - this.minPressure) / (pO2End - pO2Start);
+            pO2Start = 0.501; // needs to go above limit
+        } else if (pO2End <= this.minPressure) {
+            durationMinutes = durationMinutes * (pO2Start - this.minPressure) / (pO2Start - pO2End);
+            pO2End = 0.501;
+        }
 
-    /**
-     * Formula written in FORTRAN:
-     * OTU = 3.0/11.0*TIME/(maxPO2 - lowPO2)*(((maxPO2 - 0.5)/0.5)**(11.0/6.0) - ((lowPO2 - 0.5)/0.5)**(11.0/6.0))
-     */
-    private differenceEquation(time: number, maxPO2: number, lowPO2: number): OTU {
-        return OtuCalculator.threeElevenths * time / (maxPO2 - lowPO2) * (this.pO2part(maxPO2) - this.pO2part(lowPO2));
-    }
-
-    private pO2part(pO2: number): number {
-        return Math.pow((pO2 - .5) / .5, OtuCalculator.elevenSixths);
+        // https://thetheoreticaldiver.org/wordpress/index.php/2018/12/05/a-few-thoughts-on-oxygen-toxicity/
+        // simplified version of ((Pa + Pb) / 2 - 0.5) / 0.5
+        const pm = (pO2Start + pO2End) - 1.0;
+        const rate = Math.pow(pm, 5.0 / 6.0) * (1.0 - 5.0 * Math.pow((pO2End - pO2Start), 2) / 216 / Math.pow(pm, 2));
+        return rate * durationMinutes;
     }
 }

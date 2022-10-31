fd55c11c5d3480fa96b7c0e798ef5f89b5faea7f
JirkaPok <j.pok@post.cz>
2022-10-31 08:07:35 -0700

    Switched diver to reactive form

--- projects/planner/src/app/diver/diver.component.ts
+++ projects/planner/src/app/diver/diver.component.ts
@@ -1,6 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { faUserCog } from '@fortawesome/free-solid-svg-icons';
 import { Diver } from 'scuba-physics';
+import { InputControls } from '../shared/inputcontrols';
 import { RangeConstants, UnitConversion } from '../shared/UnitConversion';
 
 @Component({
@@ -8,12 +11,15 @@ import { RangeConstants, UnitConversion } from '../shared/UnitConversion';
     templateUrl: './diver.component.html',
     styleUrls: ['./diver.component.css']
 })
-export class DiverComponent{
+export class DiverComponent implements OnInit {
     @Input() public diver: Diver = new Diver();
     @Input() public ranges: RangeConstants;
     public icon = faUserCog;
+    public diverForm!: FormGroup;
 
-    constructor(public units: UnitConversion) {
+    constructor(private fb: FormBuilder,
+        private numberPipe: DecimalPipe,
+        public units: UnitConversion) {
         this.ranges = units.ranges;
     }
 
@@ -22,7 +28,25 @@ export class DiverComponent{
         return this.units.fromLiter(rmvMetric);
     }
 
-    public set rmv(newValue: number) {
-        this.diver.rmv = this.units.toLiter(newValue);
+    public get rmvInvalid(): boolean {
+        const rmv = this.diverForm.controls.rmv;
+        return InputControls.controlInValid(rmv);
+    }
+
+    public ngOnInit(): void {
+        this.diverForm = this.fb.group({
+            rmv: [InputControls.formatNumber(this.numberPipe, this.rmv),
+                [Validators.required, Validators.min(this.ranges.diverRmv[0]), Validators.max(this.ranges.diverRmv[1])]]
+        });
+    }
+
+    public inputChanged(): void {
+        if(this.diverForm.invalid) {
+            return;
+        }
+
+        const values = this.diverForm.value;
+        this.diver.rmv = this.units.toLiter(values.rmv);
+        // TODO fix ppO2 bindings after the ngModel is removed
     }
 }

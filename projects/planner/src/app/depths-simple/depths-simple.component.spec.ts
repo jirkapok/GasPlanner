1df9ed235c3a28c1151ecf246259f6f160d14d2a
JirkaPok <j.pok@post.cz>
2023-03-22 19:39:09 -0700

    Removed complex part from simple

--- projects/planner/src/app/depths-simple/depths-simple.component.spec.ts
+++ projects/planner/src/app/depths-simple/depths-simple.component.spec.ts
@@ -1,5 +1,4 @@
 import { DecimalPipe } from '@angular/common';
-import { DebugElement } from '@angular/core';
 import { ComponentFixture, inject, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
@@ -32,39 +31,11 @@ export class SimpleDepthsPage {
     }
 }
 
-export class ComplexDepthsPage {
-    constructor(private fixture: ComponentFixture<DepthsSimpleComponent>) { }
-
-    public get addLevelButton(): HTMLButtonElement {
-        return this.fixture.debugElement.query(By.css('#addLevel')).nativeElement as HTMLButtonElement;
-    }
-
-    public get durationDebugs(): DebugElement[] {
-        const all = this.fixture.debugElement.queryAll(By.css('#durationItem'));
-        return all;
-    }
-
-    public removeLevelButton(index: number): HTMLButtonElement {
-        const all = this.fixture.debugElement.queryAll(By.css('#removeLevel'));
-        return all[index].nativeElement as HTMLButtonElement;
-    }
-
-    public durationInput(index: number): HTMLInputElement {
-        return this.durationDebugs[index].nativeElement as HTMLInputElement;
-    }
-
-    public depthInput(index: number): HTMLInputElement {
-        const all = this.fixture.debugElement.queryAll(By.css('#depthItem'));
-        return all[index].nativeElement as HTMLInputElement;
-    }
-}
-
 describe('DepthsComponent', () => {
     let component: DepthsSimpleComponent;
     let depths: DepthsService;
     let fixture: ComponentFixture<DepthsSimpleComponent>;
     let simplePage: SimpleDepthsPage;
-    let complexPage: ComplexDepthsPage;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
@@ -87,7 +58,6 @@ describe('DepthsComponent', () => {
         component.planner.calculate();
         fixture.detectChanges();
         simplePage = new SimpleDepthsPage(fixture);
-        complexPage = new ComplexDepthsPage(fixture);
         const scheduler = TestBed.inject(DelayedScheduleService);
         spyOn(scheduler, 'schedule')
             .and.callFake(() => {
@@ -113,11 +83,12 @@ describe('DepthsComponent', () => {
                 expect(simplePage.durationInput.value).toBe('19');
             });
 
-            it('Switch to simple view', inject([ViewSwitchService], (viewSwitch: ViewSwitchService) => {
+            xit('Switch to simple view', inject([ViewSwitchService], (viewSwitch: ViewSwitchService) => {
                 viewSwitch.isComplex = true;
                 fixture.detectChanges();
-                complexPage.durationInput(1).value = '20';
-                complexPage.durationInput(1).dispatchEvent(new Event('input'));
+                // TODO makes still sense?
+                // complexPage.durationInput(1).value = '20';
+                // complexPage.durationInput(1).dispatchEvent(new Event('input'));
                 expect(depths.planDuration).toBe(21.7);
             }));
         });
@@ -129,58 +100,6 @@ describe('DepthsComponent', () => {
             simplePage.durationInput.value = 'aaa';
             simplePage.durationInput.dispatchEvent(new Event('input'));
             expect(durationSpy).not.toHaveBeenCalled();
-        });
-    });
-
-
-    describe('Complex view', () => {
-        beforeEach(() => {
-            const viewSwitch = TestBed.inject(ViewSwitchService);
-            viewSwitch.isComplex = true;
-            fixture.detectChanges();
-        });
-
-        it('Change depth calculates profile correctly', () => {
-            complexPage.durationInput(1).value = '5';
-            complexPage.durationInput(1).dispatchEvent(new Event('input'));
-            // in case of wrong binding, the algorithm ads segment with 0 duration
-            expect(component.planner.dive.totalDuration).toBe(882);
-        });
-
-        describe('Levels enforce calculation', () => {
-            it('Adds level to end of profile segments', () => {
-                complexPage.addLevelButton.click();
-                fixture.detectChanges();
-                expect(depths.levels.length).toBe(3);
-                expect(complexPage.durationDebugs.length).toBe(3);
-            });
-
-            it('Is removed from correct position', () => {
-                complexPage.removeLevelButton(1).click();
-                fixture.detectChanges();
-                expect(depths.levels.length).toBe(1);
-                expect(complexPage.durationDebugs.length).toBe(1);
-            });
-        });
-
-        describe('Invalid form prevents calculation after', () => {
-            it('wrong level enddepth', () => {
-                const durationSpy = spyOn(depths, 'levelChanged')
-                    .and.callThrough();
-
-                complexPage.durationInput(1).value = 'aaa';
-                complexPage.durationInput(1).dispatchEvent(new Event('input'));
-                expect(durationSpy).not.toHaveBeenCalled();
-            });
-
-            it('wrong level duration', () => {
-                const durationSpy = spyOn(depths, 'levelChanged')
-                    .and.callThrough();
-
-                complexPage.depthInput(1).value = 'aaa';
-                complexPage.depthInput(1).dispatchEvent(new Event('input'));
-                expect(durationSpy).not.toHaveBeenCalled();
-            });
         });
     });
 
--- projects/planner/src/app/depths-simple/depths-simple.component.ts
+++ projects/planner/src/app/depths-simple/depths-simple.component.ts
@@ -6,14 +6,12 @@ import { faLayerGroup, faPlus, faMinus } from '@fortawesome/free-solid-svg-icons
 import { takeUntil } from 'rxjs';
 import { DepthsService } from '../shared/depths.service';
 import { InputControls } from '../shared/inputcontrols';
-import { Level, Dive, TankBound } from '../shared/models';
+import { Dive } from '../shared/models';
 import { PlannerService } from '../shared/planner.service';
 import { Streamed } from '../shared/streamed';
 import { RangeConstants, UnitConversion } from '../shared/UnitConversion';
 import { ValidatorGroups } from '../shared/ValidatorGroups';
 import { Plan } from '../shared/plan.service';
-import { TanksService } from '../shared/tanks.service';
-import { ViewSwitchService } from '../shared/viewSwitchService';
 
 @Component({
     selector: 'app-depths-simple',
@@ -24,7 +22,6 @@ export class DepthsSimpleComponent extends Streamed implements OnInit {
     public cardIcon = faLayerGroup;
     public addIcon = faPlus;
     public removeIcon = faMinus;
-    public complexForm!: UntypedFormGroup;
     public simpleForm!: UntypedFormGroup;
     public dive: Dive;
 
@@ -33,9 +30,7 @@ export class DepthsSimpleComponent extends Streamed implements OnInit {
         private inputs: InputControls,
         private validators: ValidatorGroups,
         public planner: PlannerService,
-        private tanksService: TanksService,
         public depths: DepthsService,
-        private viewSwitch: ViewSwitchService,
         public units: UnitConversion,
         private plan: Plan) {
         super();
@@ -48,19 +43,6 @@ export class DepthsSimpleComponent extends Streamed implements OnInit {
         return this.units.ranges;
     }
 
-    public get isComplex(): boolean {
-        return this.viewSwitch.isComplex;
-    }
-
-    public get minimumSegments(): boolean {
-        return this.plan.minimumSegments;
-    }
-
-    // only to get their label, formatted in the tankLabel
-    public get tanks(): TankBound[] {
-        return this.tanksService.tanks;
-    }
-
     public get noDecoTime(): number {
         return this.plan.noDecoTime;
     }
@@ -70,40 +52,7 @@ export class DepthsSimpleComponent extends Streamed implements OnInit {
         return this.inputs.controlInValid(duration);
     }
 
-    public get levelControls(): UntypedFormArray {
-        return this.complexForm.controls.levels as UntypedFormArray;
-    }
-
-    public depthItemInvalid(index: number): boolean {
-        const level = this.levelControls.at(index) as UntypedFormGroup;
-        const endDepth = level.controls.endDepth;
-        return this.inputs.controlInValid(endDepth);
-    }
-
-    public durationItemInvalid(index: number): boolean {
-        const level = this.levelControls.at(index) as UntypedFormGroup;
-        const duration = level.controls.duration;
-        return this.inputs.controlInValid(duration);
-    }
-
-    public startDepth(index: number): number {
-        return this.levelAt(index).startDepth;
-    }
-
-    public tankLabelFor(index: number): string {
-        return this.levelAt(index).tankLabel;
-    }
-
-    public assignTank(index: number, tank: TankBound): void {
-        const level = this.levelAt(index);
-        this.depths.assignTank(level, tank);
-    }
-
     public ngOnInit(): void {
-        this.complexForm = this.fb.group({
-            levels: this.fb.array(this.createLevelControls())
-        });
-
         this.simpleForm = this.fb.group({
             planDuration: this.createDurationControl(this.depths.planDuration),
         });
@@ -114,50 +63,7 @@ export class DepthsSimpleComponent extends Streamed implements OnInit {
             .subscribe(() => {
                 this.depths.updateLevels();
                 this.reloadSimple();
-                this.reloadComplex();
             });
-
-        // for complex view only
-        this.viewSwitch.viewSwitched.pipe(takeUntil(this.unsubscribe$))
-            .subscribe(() => {
-                this.depths.updateLevels();
-                this.reloadComplex();
-            });
-    }
-
-    public addLevel(): void {
-        if (this.complexForm.invalid) {
-            return;
-        }
-
-        this.depths.addSegment();
-        const index = this.depths.levels.length - 1;
-        const newLevel = this.levelAt(index);
-        const levelControls = this.createLevelControl(newLevel);
-        this.levelControls.push(levelControls);
-    }
-
-    public removeLevel(index: number): void {
-        if (this.complexForm.invalid || !this.minimumSegments) {
-            return;
-        }
-
-        const level = this.levelAt(index);
-        this.depths.removeSegment(level);
-        this.levelControls.removeAt(index);
-    }
-
-    public levelChanged(index: number): void {
-        if (this.complexForm.invalid) {
-            return;
-        }
-
-        const level = this.levelAt(index);
-        const levelControl = this.levelControls.at(index) as UntypedFormGroup;
-        const levelValue = levelControl.value;
-        level.duration = Number(levelValue.duration);
-        level.endDepth = Number(levelValue.endDepth);
-        this.depths.levelChanged();
     }
 
     public durationChanged(): void {
@@ -177,34 +83,7 @@ export class DepthsSimpleComponent extends Streamed implements OnInit {
         });
     }
 
-    // for complex view only
-    private reloadComplex(): void {
-        this.levelControls.clear();
-        this.createLevelControls().forEach(c => this.levelControls.push(c));
-    }
-
-    private createLevelControls(): AbstractControl[] {
-        const created: AbstractControl[] = [];
-        for (const level of this.depths.levels) {
-            const newControl = this.createLevelControl(level);
-            created.push(newControl);
-        }
-
-        return created;
-    }
-
-    private createLevelControl(level: Level): AbstractControl {
-        return this.fb.group({
-            duration: this.createDurationControl(level.duration),
-            endDepth: [this.inputs.formatNumber(level.endDepth), this.validators.depth]
-        });
-    }
-
     private createDurationControl(duration: number): [string | null, Validators[]] {
         return [this.inputs.formatNumber(duration), this.validators.duration];
-    }
-
-    private levelAt(index: number): Level {
-        return this.depths.levels[index];
     }
 }

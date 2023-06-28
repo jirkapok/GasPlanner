5b06ccce9a492c2b363b5a318ad6360c3b728ec1
JirkaPok <j.pok@post.cz>
2023-06-28 12:41:04 -0700

    Introduced standarad tank sizes

--- projects/scuba-physics/src/lib/StandardTanks.ts
+++ projects/scuba-physics/src/lib/StandardTanks.ts
@@ -40,3 +40,114 @@ export class TankConstants {
         return Precision.round(bars, 3);
     }
 }
+
+
+export interface DefaultTank {
+    /** Common label of the cylinder */
+    name: string;
+    /** Ideal capacity in respective liters/cuft */
+    size: number;
+    /** in respective units bars/psi */
+    workingPressure: number;
+}
+
+export interface StandardTanks {
+    available: DefaultTank[];
+    primary: DefaultTank;
+    stage: DefaultTank;
+}
+
+export class ImperialTanks implements StandardTanks {
+    public get available(): DefaultTank[] {
+        return [
+            this.primary,
+            this.stage,
+            {
+                name: '2xLP85',
+                size: 165,
+                workingPressure: 2640,
+            },
+            {
+                name: 'S40',
+                size: 41.3,
+                workingPressure: 3000,
+            },
+            {
+                name: 'LP85',
+                size: 82.5,
+                workingPressure: 2640,
+            },
+            {
+                name: 'HP80',
+                size: 84.4,
+                workingPressure: 3442,
+            },
+            {
+                name: 'HP100',
+                size: 106.7,
+                workingPressure: 3442,
+            }
+        ];
+    }
+
+    public get primary(): DefaultTank {
+        return {
+            name: 'HP117',
+            size: TankConstants.imperialTankSize,
+            workingPressure: TankConstants.imperialTankWorkPressure,
+        };
+    }
+
+    public get stage(): DefaultTank {
+        return {
+            name: 'S80',
+            size: TankConstants.imperialStageSize,
+            workingPressure: TankConstants.imperialStageWorkPressure,
+        };
+    }
+}
+
+export class MetricTanks implements StandardTanks {
+    public get available(): DefaultTank[] {
+        return [
+            this.primary,
+            this.stage,
+            {
+                name: '2x12 L',
+                size: 24,
+                workingPressure: 0,
+            },
+            {
+                name: 'S40',
+                size: 5.7,
+                workingPressure: 0,
+            },
+            {
+                name: '10L',
+                size: 10,
+                workingPressure: 0,
+            },
+            {
+                name: '12 L',
+                size: 12,
+                workingPressure: 0,
+            }
+        ];
+    }
+
+    public get primary(): DefaultTank {
+        return {
+            name: '15 L',
+            size: TankConstants.mertricTankSize,
+            workingPressure: 0,
+        };
+    }
+
+    public get stage(): DefaultTank {
+        return {
+            name: 'S80',
+            size: TankConstants.metricStageSize,
+            workingPressure: 0,
+        };
+    }
+}

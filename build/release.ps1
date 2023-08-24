f126e14ddbbb6cf54f92d927c9edf1eecc722252
JirkaPok <j.pok@post.cz>
2023-08-24 03:16:32 -0700

    Implemented release script

--- build/release.ps1
+++ build/release.ps1
@@ -1,9 +1,38 @@
-# use this script as automation for release using git
+# use this script as automation for release at github
+param([String]$tag)
+Write-Host "Preparing version $($tag)"
 
 git checkout master
 git merge develop
 
-# ./build.ps1
+./build.ps1
 
 git checkout gh-pages
 
+# merge files
+$filesToReplace = @(
+    "*.js",
+    "*.txt",
+    "*.html",
+    "*.css",
+    "favicon.ico",
+    "manifest.webmanifest",
+    "ngsw.json"
+)
+
+Remove-Item $filesToReplace -Force
+Remove-Item "assets" -Recurse -Force
+Copy-Item "dist\planner\*" . -Recurse
+Copy-Item "index.html" "404.html" -Recurse
+
+# Release commit
+git add .
+git commit -m "Release $($tag)"
+
+# this kick of the pages deployment on github pages
+git push
+
+git checkout master
+git push
+git tag $tag
+git push --tags

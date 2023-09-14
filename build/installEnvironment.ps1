e275c9bc6bd0641d9a219144e0a320dd93c57913
Jiri Pokorny <jiri.pokorny@solarwinds.com>
2023-09-15 00:53:21 +0200

    Updated install script

--- build/installEnvironment.ps1
+++ build/installEnvironment.ps1
@@ -2,8 +2,8 @@
 
 # NodeJs
 choco install nvm -y
-nvm install -lts
-nvm use -lts
+nvm install lts
+nvm use lts
 npm install -g @angular/cli
 npm install -g typescript
 
@@ -12,9 +12,6 @@ choco install vscode -y
 # Optional extensions
 # code --install-extension ms-vscode.powershell
 # code --install-extension angular.ng-template
-# code --install-extension hbenl.vscode-test-explorer
-# code --install-extension lucono.karma-test-explorer
-# code --install-extension hbenl.vscode-jasmine-test-adapter
 
 # NPM Packages
 .\build.ps1

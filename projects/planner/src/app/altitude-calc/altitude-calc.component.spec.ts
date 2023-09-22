d67b0abd1800c94346cd6b15ac0310b54663f2c3
Jiri Pokorny <jiri.pokorny@solarwinds.com>
2023-09-22 17:33:10 +0200

    Fixed broken altitude test

--- projects/planner/src/app/altitude-calc/altitude-calc.component.spec.ts
+++ projects/planner/src/app/altitude-calc/altitude-calc.component.spec.ts
@@ -5,6 +5,17 @@ import { UnitConversion } from '../shared/UnitConversion';
 import { ValidatorGroups } from '../shared/ValidatorGroups';
 import { InputControls } from '../shared/inputcontrols';
 import { DecimalPipe } from '@angular/common';
+import { SubViewStorage } from '../shared/subViewStorage';
+import { ViewStates } from '../shared/viewStates';
+import { PreferencesStore } from '../shared/preferencesStore';
+import { PlannerService } from '../shared/planner.service';
+import { WorkersFactoryCommon } from '../shared/serial.workers.factory';
+import { TanksService } from '../shared/tanks.service';
+import { Plan } from '../shared/plan.service';
+import { OptionsService } from '../shared/options.service';
+import { WayPointsService } from '../shared/waypoints.service';
+import { Preferences } from '../shared/preferences';
+import { ViewSwitchService } from '../shared/viewSwitchService';
 
 describe('AltitudeCalcComponent', () => {
     let component: AltitudeCalcComponent;
@@ -15,7 +26,10 @@ describe('AltitudeCalcComponent', () => {
             declarations: [AltitudeCalcComponent],
             providers: [
                 UnitConversion, ValidatorGroups, InputControls,
-                DecimalPipe
+                DecimalPipe, SubViewStorage, ViewStates,
+                PreferencesStore, PlannerService, WorkersFactoryCommon,
+                TanksService, Plan, OptionsService, WayPointsService,
+                Preferences, ViewSwitchService
             ]
         });
         fixture = TestBed.createComponent(AltitudeCalcComponent);

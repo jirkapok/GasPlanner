82633604a8fa8d045484b4310d04e905a707d930
Jiri Pokorny <jiri.pokorny@solarwinds.com>
2023-09-27 22:24:45 +0200

    Removed install lib from build script

--- build/build.ps1
+++ build/build.ps1
@@ -1,18 +1,14 @@
 # clean
 
-if (Test-Path dist) { 
+if (Test-Path dist) {
     Remove-Item dist -Recurse -Force
 }
 
-if (Test-Path node_modules) { 
-    Remove-Item node_modules -Recurse -Force 
+if (Test-Path node_modules) {
+    Remove-Item node_modules -Recurse -Force
 }
 
 # Compile
 npm install
 npm run build-lib
-
-# that is an optional dependency, this sequence
-# is work around for not wokring npm --no-optional switch
-npm install dist/scuba-physics --no-package-lock 
 npm run build

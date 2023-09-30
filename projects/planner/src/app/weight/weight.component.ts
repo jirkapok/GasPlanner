52acedcea195581b44c5cf7de69a35a5a9856147
Jiri Pokorny <jiri.pokorny@solarwinds.com>
2023-09-30 16:41:18 +0200

    Replaced weight form controls

--- projects/planner/src/app/weight/weight.component.ts
+++ projects/planner/src/app/weight/weight.component.ts
@@ -7,13 +7,13 @@ import {
 } from '@angular/forms';
 import { ValidatorGroups } from '../shared/ValidatorGroups';
 import { InputControls } from '../shared/inputcontrols';
-import { AltitudeForm } from '../altitude/altitude.component';
-import { Precision, AltitudeCalculator } from 'scuba-physics';
+import { Precision, AltitudeCalculator, Tank, TankTemplate } from 'scuba-physics';
 import { KnownViews } from '../shared/viewStates';
 import { AltitudeViewState } from '../shared/serialization.model';
 import { SubViewStorage } from '../shared/subViewStorage';
+import { TankBound } from '../shared/models';
 
-interface AltitudeDepthForm extends AltitudeForm {
+interface WeightForm {
     pressure: FormControl<number>;
     actualDepth: FormControl<number>;
 }
@@ -25,8 +25,9 @@ interface AltitudeDepthForm extends AltitudeForm {
 })
 export class WeightCalcComponent implements OnInit {
     public calcIcon = faCalculator;
-    public altitudeForm!: FormGroup<AltitudeDepthForm>;
+    public weightForm!: FormGroup<WeightForm>;
     public calc = new AltitudeCalculator();
+    public tank: TankBound;
 
     constructor(
         private fb: NonNullableFormBuilder,
@@ -35,6 +36,7 @@ export class WeightCalcComponent implements OnInit {
         public units: UnitConversion,
         public location: Location,
         private viewStates: SubViewStorage) {
+        this.tank = new TankBound(Tank.createDefault(), this.units);
         this.loadState();
         this.saveState();
     }
@@ -43,13 +45,15 @@ export class WeightCalcComponent implements OnInit {
         return this.units.ranges;
     }
 
-    public get pressureInvalid(): boolean {
-        const pressureControl = this.altitudeForm.controls.pressure;
+    // TODO
+    public get workPressureInvalid(): boolean {
+        const pressureControl = this.weightForm.controls.pressure;
         return this.inputs.controlInValid(pressureControl);
     }
 
-    public get actualDepthInvalid(): boolean {
-        const depthControl = this.altitudeForm.controls.actualDepth;
+    // TODO
+    public get pressureInvalid(): boolean {
+        const depthControl = this.weightForm.controls.actualDepth;
         return this.inputs.controlInValid(depthControl);
     }
 
@@ -73,72 +77,87 @@ export class WeightCalcComponent implements OnInit {
         return Precision.round(depth, 2);
     }
 
+    public firstTankSizeInvalid(): boolean {
+        return false; // TODO tank size invalid
+    }
+
     public ngOnInit(): void {
-        this.altitudeForm = this.fb.group({
+        this.weightForm = this.fb.group({
             pressure: [this.calcPressure, this.validators.rangeFor(this.ranges.altitudePressure)],
-            altitude: [this.calcAltitude, this.validators.altitude],
             actualDepth: [this.calcAltitudeDepth, this.validators.depth]
         });
     }
 
+    public sizeChanged(): void {
+        // TODO sizeChanged
+    }
+
+    public applyTemplate(template: TankTemplate): void {
+        // TODO apply tank size template
+    }
+
+    // TODO replace
     public pressureChanged(): void {
-        if (this.altitudeForm.invalid) {
+        if (this.weightForm.invalid) {
             return;
         }
 
-        const values = this.altitudeForm.value;
+        const values = this.weightForm.value;
         const metricPressure = this.units.toBar(Number(values.pressure));
         this.calc.pressure = metricPressure;
 
-        this.altitudeForm.patchValue({
-            altitude: this.calcAltitude
-        });
+        // this.weightForm.patchValue({
+        //     altitude: this.calcAltitude
+        // });
 
         this.saveState();
     }
 
+    // TODO replace
     public altitudeChanged(newValue: number): void {
-        if (this.altitudeForm.invalid) {
+        if (this.weightForm.invalid) {
             return;
         }
 
         // already in metric
-        this.calc.altitude = newValue;
+        // this.calc.altitude = newValue;
         this.saveState();
     }
 
+    // TODO replace
     public inputChanged(): void {
-        if (this.altitudeForm.invalid) {
+        if (this.weightForm.invalid) {
             return;
         }
 
-        const values = this.altitudeForm.value;
+        const values = this.weightForm.value;
         const metricDepth = this.units.toMeters(Number(values.actualDepth));
-        this.calc.altitudeDepth = metricDepth;
+        // this.calc.altitudeDepth = metricDepth;
         this.saveState();
     }
 
+    // TODO implement save/load of state
     private loadState(): void {
-        let state: AltitudeViewState = this.viewStates.loadView(KnownViews.altitude);
+        // let state: AltitudeViewState = this.viewStates.loadView(KnownViews.altitude);
 
-        if (!state) {
-            state = this.createState();
-        }
+        // if (!state) {
+        //     state = this.createState();
+        // }
 
-        this.calc.altitudeDepth = state.actualDepth;
-        this.calc.altitude = state.altitude;
+        // this.calc.altitudeDepth = state.actualDepth;
+        // this.calc.altitude = state.altitude;
     }
 
     private saveState(): void {
-        const viewState = this.createState();
-        this.viewStates.saveView(viewState);
+    //     const viewState = this.createState();
+    //     this.viewStates.saveView(viewState);
     }
 
-    private createState(): AltitudeViewState {
-        return {
-            altitude: this.calc.altitude,
-            actualDepth: this.calc.altitudeDepth,
-            id: KnownViews.altitude
-        };
-    }
+    // private createState(): AltitudeViewState {
+    //     return {
+    //         altitude: this.calc.altitude,
+    //         actualDepth: this.calc.altitudeDepth,
+    //         id: KnownViews.altitude
+    //     };
+    // }
 }

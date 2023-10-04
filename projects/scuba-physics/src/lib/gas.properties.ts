c29793b7cd474f93475585f226e92e574f4c9bf4
Jiri Pokorny <jiri.pokorny@solarwinds.com>
2023-10-04 16:16:56 +0200

    Added documentation to gas properties

--- projects/scuba-physics/src/lib/gas.properties.ts
+++ projects/scuba-physics/src/lib/gas.properties.ts
@@ -3,9 +3,9 @@ import { Gas, GasMixtures } from './Gases';
 import { Tank } from './Tanks';
 import { DepthConverter } from './depth-converter';
 
-// TODO add documentation to all properties
+/** Calculator for gas properties of selected mix at defined depth. */
 export class GasProperties {
-    /** in meters */
+    /** Gets or sets current in meters */
     public depth = 0;
     private readonly _tank = Tank.createDefault();
     private readonly depthConverter = DepthConverter.simple();
@@ -15,37 +15,44 @@ export class GasProperties {
         return this._tank;
     }
 
+    /** Gets current mixture oxygen partial pressure */
     public get ppO2(): number {
         const result = GasMixtures.partialPressure(this.depthPressure, this.gas.fO2);
         return result;
     }
 
+    /** Gets current mixture helium partial pressure */
     public get ppHe(): number {
         const result = GasMixtures.partialPressure(this.depthPressure, this.gas.fHe);
         return result;
     }
 
+    /** Gets current mixture nitrogen partial pressure */
     public get ppN2(): number {
         const result = GasMixtures.partialPressure(this.depthPressure, this.gas.fN2);
         return result;
     }
 
+    /** Gets total pressure of the mix at current depth. Its value equals to current depth pressure in bars. */
     public get totalPp(): number {
         return this.depthPressure;
     }
 
+    /** Gets minimum operational depth in meters based on current maximum partial pressure and oxygen content. */
     public get minDepth(): number {
         const surface = this.depthConverter.surfacePressure;
         const minDepthPressure = GasMixtures.ceiling(this.gas.fO2, surface);
         return this.depthConverter.fromBar(minDepthPressure);
     }
 
+    /** Gets maximum operational depth in meters based on current maximum partial pressure and oxygen content. */
     public get maxDepth(): number {
         const maxppO2 = 1.4; // TODO add to UI
         const maxDepthPressure = GasMixtures.mod(1.4, this.gas.fO2);
         return this.depthConverter.fromBar(maxDepthPressure);
     }
 
+    /** Gets current mix equivalent narcotic depth based on current depth. */
     public get end(): number {
         let endDepthPressure = GasMixtures.end(this.depthPressure, this.gas.fN2, this.gas.fO2);
 
@@ -56,12 +63,14 @@ export class GasProperties {
         return this.depthConverter.fromBar(endDepthPressure);
     }
 
+    /** Gets maximum narcotic depth of current mixture. */
     public get mnd(): number {
         const narcDepthBars = 4; // TODO add to UI
         const endDepthPressure = GasMixtures.mnd(narcDepthBars, this.gas.fN2, this.gas.fO2);
         return this.depthConverter.fromBar(endDepthPressure);
     }
 
+    /** Gets density in g/l of the mixture at current depth. */
     public get density(): number {
         const density = this.densityCalc.atDepth(this.gas, this.depth);
         return density;

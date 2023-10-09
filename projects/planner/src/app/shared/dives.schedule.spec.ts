305269a50b3cf1fa35c7bc10615fbbe4099c3e70
Jiri Pokorny <jiri.pokorny@solarwinds.com>
2023-10-09 17:28:03 +0200

    Added initial repetitive dives ScheduledDives service

---
+++ projects/planner/src/app/shared/dives.schedule.spec.ts
@@ -0,0 +1,49 @@
+import { UnitConversion } from './UnitConversion';
+import { DivesSchedule } from './dives.schedule';
+
+describe('Scheduled dives', () => {
+    let sut: DivesSchedule;
+
+    beforeEach(() => {
+        sut = new DivesSchedule(new UnitConversion());
+    });
+
+    it('Has default dive', () => {
+        expect(sut.dives.length).toEqual(1);
+    });
+
+    describe('Added dive', () => {
+        beforeEach(() => {
+            sut.add();
+        });
+
+        it('is added to the list', () => {
+            expect(sut.dives.length).toEqual(2);
+        });
+
+        it('is added as First dive', () => {
+            expect(sut.dives[1].surfaceInterval).toBeUndefined();
+        });
+
+        it('has title', () => {
+            expect(sut.dives[1].title).toEqual('12 min/30 m');
+        });
+    });
+
+    it('Dive is removed from the list', () => {
+        sut.add();
+        sut.add();
+        const second = sut.dives[1];
+        sut.remove(second);
+
+        expect(sut.dives.length).toEqual(2);
+        expect(sut.dives.includes(second)).toBeFalsy();
+    });
+
+    // TODO planner.isNdlValid needs to be removed from planner
+    // TODO depthService.applyMaxDuration needs to be removed from depthsService to be able remove planner dependency
+    // TODO Scheduled dives test cases:
+    // * any change in dive list triggers save preferences
+    // * New dive is loaded from default dive
+    // * Dives are loaded from saved storage
+});
---
+++ projects/planner/src/app/shared/dives.schedule.ts
@@ -0,0 +1,61 @@
+import { Tank } from 'scuba-physics';
+import { UnitConversion } from './UnitConversion';
+import { OptionsService } from './options.service';
+import { Plan } from './plan.service';
+import { TanksService } from './tanks.service';
+
+export class DiveSchedule {
+    /** In minutes or undefined in case it is the first dive */
+    public surfaceInterval?: number;
+    private optionsService: OptionsService;
+    private _tanks: TanksService;
+    private plan: Plan = new Plan(); // Depths service
+
+    constructor(private units: UnitConversion) {
+        this._tanks = new TanksService(units);
+        this.optionsService = new OptionsService(this.units);
+
+        // this enforces to initialize the levels, needs to be called after subscribe to plan
+        if (this.plan.maxDepth === 0) {
+            let requiredDepth = this.units.defaults.stopsDistance * 10; // 30 m or 100 ft
+            requiredDepth = this.units.toMeters(requiredDepth);
+            const firstTank = this.firstTank;
+            const options = this.optionsService.getOptions();
+            this.plan.setSimple(requiredDepth, 12, firstTank, options);
+        }
+    }
+
+    public get title(): string {
+        return `${this.plan.duration} min/${this.plan.maxDepth} m`;
+    }
+
+    private get firstTank(): Tank {
+        return this._tanks.firstTank.tank;
+    }
+}
+
+export class DivesSchedule {
+    private _dives: DiveSchedule[] = [
+    ];
+
+    constructor(private units: UnitConversion) {
+        this.add();
+    }
+
+    public get dives(): DiveSchedule[] {
+        return this._dives.slice();
+    }
+
+    public add(): void {
+        const newDive = this.createDiveSchedule();
+        this._dives.push(newDive);
+    }
+
+    public remove(dive: DiveSchedule): void {
+        this._dives = this._dives.filter(g => g !== dive);
+    }
+
+    private createDiveSchedule(): DiveSchedule {
+        return new DiveSchedule(this.units);
+    }
+}

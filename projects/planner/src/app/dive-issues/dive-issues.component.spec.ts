3e21078697269076a9cab0d3c267e0f7af660a3e
Jiri Pokorny <jiri.pokorny@solarwinds.com>
2023-10-10 22:28:40 +0200

    fixed dive issues test setup

--- projects/planner/src/app/dive-issues/dive-issues.component.spec.ts
+++ projects/planner/src/app/dive-issues/dive-issues.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DiveIssuesComponent } from './dive-issues.component';
+import { Plan } from '../shared/plan.service';
+import { UnitConversion } from '../shared/UnitConversion';
+import { DiveResults } from '../shared/diveresults';
 
 describe('DiveIssuesComponent', () => {
     let component: DiveIssuesComponent;
@@ -8,7 +11,10 @@ describe('DiveIssuesComponent', () => {
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            declarations: [DiveIssuesComponent]
+            declarations: [DiveIssuesComponent],
+            providers: [
+                DiveResults, UnitConversion, Plan
+            ]
         });
         fixture = TestBed.createComponent(DiveIssuesComponent);
         component = fixture.componentInstance;

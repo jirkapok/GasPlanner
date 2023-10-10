7693857dbe5da8a4f63057ecab0ab4c21a84a705
Jiri Pokorny <jiri.pokorny@solarwinds.com>
2023-10-10 22:09:08 +0200

    Fixed diveinfo tests di setup

--- projects/planner/src/app/diveinfo/diveinfo.component.spec.ts
+++ projects/planner/src/app/diveinfo/diveinfo.component.spec.ts
@@ -1,6 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DiveInfoComponent } from './diveinfo.component';
+import { DepthsService } from '../shared/depths.service';
+import { UnitConversion } from '../shared/UnitConversion';
+import { TanksService } from '../shared/tanks.service';
+import { DelayedScheduleService } from '../shared/delayedSchedule.service';
+import { PlannerService } from '../shared/planner.service';
+import { WorkersFactoryCommon } from '../shared/serial.workers.factory';
+import { Plan } from '../shared/plan.service';
+import { DiveResults } from '../shared/diveresults';
+import { OptionsService } from '../shared/options.service';
+import { WayPointsService } from '../shared/waypoints.service';
+import { SubViewStorage } from '../shared/subViewStorage';
+import { ViewStates } from '../shared/viewStates';
+import { PreferencesStore } from '../shared/preferencesStore';
+import { Preferences } from '../shared/preferences';
+import { ViewSwitchService } from '../shared/viewSwitchService';
 
 describe('DiveInfoComponent', () => {
     let component: DiveInfoComponent;
@@ -8,7 +23,17 @@ describe('DiveInfoComponent', () => {
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            declarations: [DiveInfoComponent]
+            declarations: [
+                DiveInfoComponent,
+            ],
+            providers: [
+                DepthsService, UnitConversion, TanksService,
+                DelayedScheduleService, PlannerService,
+                WorkersFactoryCommon, Plan, DiveResults,
+                OptionsService, WayPointsService, SubViewStorage,
+                ViewStates, PreferencesStore, Preferences,
+                ViewSwitchService
+            ]
         });
         fixture = TestBed.createComponent(DiveInfoComponent);
         component = fixture.componentInstance;

682da1a873638b9f6cdaef418477e1af031925cf
Jiri Pokorny <jiri.pokorny@solarwinds.com>
2023-10-24 02:08:19 +0200

    First skeleton of redundancies calculator UI

--- projects/planner/src/app/redundancies/redundancies.component.ts
+++ projects/planner/src/app/redundancies/redundancies.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { faCalculator } from '@fortawesome/free-solid-svg-icons';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { RangeConstants, UnitConversion } from '../shared/UnitConversion';
+import { TankBound } from '../shared/models';
+import { Tank, TankTemplate } from 'scuba-physics';
 
 @Component({
     selector: 'app-redundancies',
@@ -6,5 +12,33 @@ import { Component } from '@angular/core';
     styleUrls: ['./redundancies.component.scss']
 })
 export class RedundanciesComponent {
+    public calcIcon = faCalculator;
+    public redForm!: FormGroup<any>;
+    public tankA: TankBound;
 
+    constructor(public location: Location,
+        public units: UnitConversion,
+        private fb: FormBuilder) {
+        this.tankA = new TankBound(Tank.createDefault(), this.units);
+        this.redForm = this.fb.group([]);
+    }
+
+    public get ranges(): RangeConstants {
+        return this.units.ranges;
+    }
+
+    public get finalPressure(): number {
+        return 0;
+    }
+
+    public get tankASizeInvalid(): boolean {
+        return false;
+    }
+
+    public applyTemplate(template: TankTemplate): void  {
+    }
+
+    public inputChanged(): void {
+
+    }
 }

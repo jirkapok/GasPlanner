fdbcad6e88c27e07251a2531b04d76a1b07db5c8
Jiri Pokorny <j.pok@post.cz>
2023-12-11 17:02:45 +0100

    Added repetitive dive test cases

--- projects/scuba-physics/src/lib/BuhlmannAlgortihm.repetitive.spec.ts
+++ projects/scuba-physics/src/lib/BuhlmannAlgortihm.repetitive.spec.ts
@@ -1,6 +1,8 @@
 import _ from 'lodash';
 import { Time } from './Time';
-import { AlgorithmParams, BuhlmannAlgorithm, SurfaceIntervalParameters } from './BuhlmannAlgorithm';
+import {
+    AlgorithmParams, BuhlmannAlgorithm, RestingParameters, SurfaceIntervalParameters
+} from './BuhlmannAlgorithm';
 import { LoadedTissue, Tissues } from './Tissues';
 import { Precision } from './precision';
 import { Gases, StandardGases } from './Gases';
@@ -10,17 +12,30 @@ import { Options } from './Options';
 describe('Buhlmann Algorithm - Repetitive dives', () => {
     const sut = new BuhlmannAlgorithm();
 
-    const firstDiveOnTrimix = () => {
+    const createDiveParameters = (depth: number, rest?: RestingParameters) => {
         const gases = new Gases();
         gases.add(StandardGases.trimix1845);
         const segments = new Segments();
-        segments.add(0, 20, StandardGases.trimix1845, Time.oneMinute * 2);
-        segments.addFlat(40, StandardGases.trimix1845, Time.oneMinute * 60);
+        segments.add(0, depth, StandardGases.trimix1845, Time.oneMinute * 2);
+        segments.addFlat(depth, StandardGases.trimix1845, Time.oneMinute * 60);
         const options = new Options(1, 1, 1.6, 1.6);
-        const parameters = AlgorithmParams.forMultilevelDive(segments, gases, options);
-        const diveResult = sut.decompression(parameters);
-        return diveResult;
+        return AlgorithmParams.forMultilevelDive(segments, gases, options, rest);
     };
+
+    const diveOnTrimix = (rest?: RestingParameters) => {
+        const parameters = createDiveParameters(40, rest);
+        return sut.decompression(parameters);
+    };
+
+    const depths: number[] =  Array.from(
+        { length: 11 },
+        (v, index) => 12 + index * 3
+    );
+
+    const noDecoLimits = (rest?: RestingParameters): number[] => _(depths).map(depth => {
+        const parameters = createDiveParameters(depth, rest);
+        return sut.noDecoLimit(parameters);
+    }).value();
 
     const toTissueResult = (loaded: LoadedTissue[]) => _(loaded)
         .map(t => ({
@@ -79,7 +94,7 @@ describe('Buhlmann Algorithm - Repetitive dives', () => {
         });
 
         it('Adapts helium loading.', () => {
-            const diveResult = firstDiveOnTrimix();
+            const diveResult = diveOnTrimix();
             const r1 = toTissueResult(diveResult.tissues);
             const result2 = applySurfaceInterval(diveResult.tissues, 0, Time.oneMinute * 10);
             const r2 = toTissueResult(result2);
@@ -89,15 +104,29 @@ describe('Buhlmann Algorithm - Repetitive dives', () => {
         });
     });
 
-    // TODO Buhlmann repetitive dive test cases:
-    // * Tissues are loaded after dive (all values higher then at beginning)
-    // * no deco is lower, if there is residual nitrogen from previous dive
-
     xdescribe('Following dive', () => {
         it('Has higher tissues loading', () => {
-            const firstDive = firstDiveOnTrimix();
-            const secondDive = firstDiveOnTrimix();
-            expect(firstDive).toEqual(secondDive);
+            const firstDive = diveOnTrimix();
+            const restingParameters = new RestingParameters(firstDive.tissues, Time.oneMinute * 5);
+            const secondDive = diveOnTrimix(restingParameters);
+
+            const firstTissues = toTissueResult(firstDive.tissues);
+            const secondTissues = toTissueResult(secondDive.tissues);
+
+            expect(_(firstTissues).every((item, index) =>
+                item.pHe < secondTissues[index].pHe && item.pN2 < secondTissues[index].pN2 &&
+                item.pHe !== 0 && secondTissues[index].pHe !== 0
+            )).toBeTruthy();
+        });
+
+        it('Nodeco limit are lower', () => {
+            const firstDive = diveOnTrimix();
+            const firstDiveNdl = noDecoLimits();
+            const restingParameters = new RestingParameters(firstDive.tissues, Time.oneMinute * 5);
+            const secondDiveNdl = noDecoLimits(restingParameters);
+
+            expect(_(firstDiveNdl).every((item, index) => item < secondDiveNdl[index] ))
+                .toBeTruthy();
         });
     });
 });

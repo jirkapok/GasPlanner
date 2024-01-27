b4def78f805e59e556a27704a11a3f7f94318c57
Denis Lebo <lebo.denis@outlook.com>
2024-01-27 17:40:17 +0100

    Add code from original diveinfo component

--- projects/planner/src/app/diff/diveinfo/diff-diveinfo.component.spec.ts
+++ projects/planner/src/app/diff/diveinfo/diff-diveinfo.component.spec.ts
@@ -1,6 +1,11 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { DiveInfoDifferenceComponent } from './diff-diveinfo.component';
+import {DiveInfoDifferenceComponent} from './diff-diveinfo.component';
+import {ViewSwitchService} from '../../shared/viewSwitchService';
+import {UnitConversion} from '../../shared/UnitConversion';
+import {ProfileComparatorService} from '../../shared/profileComparatorService';
+import {DiveSchedules} from '../../shared/dive.schedules';
+import {ReloadDispatcher} from '../../shared/reloadDispatcher';
 
 describe('DiveInfoDifferenceComponent', () => {
     let component: DiveInfoDifferenceComponent;
@@ -8,7 +13,14 @@ describe('DiveInfoDifferenceComponent', () => {
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            declarations: [DiveInfoDifferenceComponent]
+            declarations: [DiveInfoDifferenceComponent],
+            providers: [
+                ViewSwitchService,
+                DiveSchedules,
+                ReloadDispatcher,
+                UnitConversion,
+                ProfileComparatorService
+            ]
         });
         fixture = TestBed.createComponent(DiveInfoDifferenceComponent);
         component = fixture.componentInstance;

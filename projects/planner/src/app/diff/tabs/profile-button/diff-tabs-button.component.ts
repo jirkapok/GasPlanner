3e849c39dfd848351051e69a29755f3495a26358
Denis Lebo <lebo.denis@outlook.com>
2024-02-15 00:52:34 +0100

    Add color toggling when button clicked

--- projects/planner/src/app/diff/tabs/profile-button/diff-tabs-button.component.ts
+++ projects/planner/src/app/diff/tabs/profile-button/diff-tabs-button.component.ts
@@ -7,4 +7,25 @@ import {Component, Input} from '@angular/core';
 })
 export class DiffTabsButtonComponent {
     @Input({required: true}) index = 0;
+    buttonFill = 'btn-outline-primary';
+    private enabled = false;
+
+    public clicked() {
+        if(this.enabled){
+            this.disableProfile();
+            return;
+        }
+
+        this.enableProfile();
+    }
+
+    private enableProfile() {
+        this.buttonFill = 'btn-primary';
+        this.enabled = true;
+    }
+
+    private disableProfile() {
+        this.buttonFill = 'btn-outline-primary';
+        this.enabled = false;
+    }
 }

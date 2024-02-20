1a6f71ffa6079466e13c94fdcb3dd5db5e57bcef
Jiri Pokorny <j.pok@post.cz>
2024-02-20 23:47:41 +0100

    Added more gas blender UI

--- projects/planner/src/app/gas-blender/gas-blender.component.ts
+++ projects/planner/src/app/gas-blender/gas-blender.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { faCalculator } from '@fortawesome/free-solid-svg-icons';
 import { FormGroup, NonNullableFormBuilder } from '@angular/forms';
+import { RangeConstants, UnitConversion } from '../shared/UnitConversion';
 
 @Component({
     selector: 'app-gas-blender',
@@ -11,7 +12,25 @@ export class GasBlenderComponent {
     public readonly calcIcon = faCalculator;
     public blenderForm!: FormGroup;
 
-    constructor(private fb: NonNullableFormBuilder) {
+    constructor(
+        public units: UnitConversion,
+        private fb: NonNullableFormBuilder) {
         this.blenderForm = this.fb.group([]);
     }
+
+    public get ranges(): RangeConstants {
+        return this.units.ranges;
+    }
+
+    // TODO Gas blender component:
+    // note we dont need working pressure, since dont count with volume only percentage
+    // create and bind service with tests
+    // add input controls and bind default values
+    // On inputs change
+    // On source mix template change
+    // On top mix template change
+    // On target mix template change
+    // Do we need Reload?
+    // bind results
+    // save/load state
 }

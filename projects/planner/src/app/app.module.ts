3db914ca853da3fe9012664adbcbab4661d3349e
Denis Lebo <lebo.denis@outlook.com>
2024-02-20 01:36:37 +0100

    Rename diff/diveresults/results folder to */table

--- projects/planner/src/app/app.module.ts
+++ projects/planner/src/app/app.module.ts
@@ -92,7 +92,7 @@ import {TestDataInjector} from './diff/testData/testDataInjector';
 import {ProfileComparatorService} from './shared/profileComparatorService';
 import {LoadTestDataComponent} from './diff/testData/loadtestdata/loadtestdata.component';
 import {WaypointsDifferenceService} from './shared/waypoints-difference.service';
-import {DiveResultsResultsDifferenceComponent} from './diff/diveresults/results/diff-diveresults-results.component';
+import {DiveResultsResultsDifferenceComponent} from './diff/diveresults/table/diff-diveresults-results.component';
 import { GasBlenderComponent } from './gas-blender/gas-blender.component';
 import { GasBlenderService } from './shared/gas-blender.service';
 import {DiffTabsButtonComponent} from './diff/tabs/profile-button/diff-tabs-button.component';
@@ -140,7 +140,6 @@ const COMPONENTS = [
     MainMenuComponent,
     NdlLimitsComponent,
     NitroxComponent,
-    ProfileDifferenceChartComponent,
     ProfileChartComponent,
     OxygenComponent,
     OxygenDropDownComponent,

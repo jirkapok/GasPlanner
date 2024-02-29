6d1953be19ac3a7dc13363f0efea1707c02b1c75
Denis Lebo <lebo.denis@outlook.com>
2024-02-29 18:33:51 +0100

    Join same gasses across profiles A/B

--- projects/planner/src/app/diff/gasconsumed/diff-gas-consumed/diff-gas-consumed.component.ts
+++ projects/planner/src/app/diff/gasconsumed/diff-gas-consumed/diff-gas-consumed.component.ts
@@ -2,6 +2,13 @@ import {Component} from '@angular/core';
 import {GasToxicity, Tank} from 'scuba-physics';
 import {faSlidersH} from '@fortawesome/free-solid-svg-icons';
 import {ProfileComparatorService} from '../../../shared/profileComparatorService';
+import {IConsumedMix, StandardGases} from 'scuba-physics';
+
+interface IMixedTanksForComparison {
+    profileA: IConsumedMix;
+    profileB: IConsumedMix;
+}
+
 
 @Component({
     selector: 'app-diff-gas-consumed',
@@ -20,5 +27,31 @@ export class GasConsumedDifferenceComponent {
 
     public get toxicity(): GasToxicity {
         return this.profileComparatorService.profileA.optionsService.toxicity;
+    }
+
+    public get getMixedTanks(): IMixedTanksForComparison[] {
+        const mixedTanks: IMixedTanksForComparison[] = [];
+
+        const emptyTank: IConsumedMix = {
+            gas: StandardGases.air.copy(),
+            total: 0,
+            consumed: 0,
+            reserve: 0
+        };
+
+        for (const tankA of this.profileComparatorService.profileACombinedTanks) {
+            const tankB: IConsumedMix | undefined = this.profileComparatorService.profileBCombinedTanks
+                .find(t => t.gas.contentCode() === tankA.gas.contentCode());
+            mixedTanks.push({ profileA: tankA, profileB: tankB ?? emptyTank });
+        }
+
+        for (const tankB of this.profileComparatorService.profileBCombinedTanks) {
+            if (!mixedTanks.find(mt => mt.profileB?.gas.contentCode() === tankB.gas.contentCode()
+            )){
+                mixedTanks.push({ profileA: emptyTank, profileB: tankB });
+            }
+        }
+
+        return mixedTanks;
     }
 }

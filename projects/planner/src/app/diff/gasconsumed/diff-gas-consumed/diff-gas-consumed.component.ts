8aac5629f71ad00a090c654fc44054ea2f34786e
Denis Lebo <lebo.denis@outlook.com>
2024-02-29 18:34:52 +0100

    Rework tank chart to work with combined gases

--- projects/planner/src/app/diff/gasconsumed/diff-gas-consumed/diff-gas-consumed.component.ts
+++ projects/planner/src/app/diff/gasconsumed/diff-gas-consumed/diff-gas-consumed.component.ts
@@ -1,5 +1,4 @@
 import {Component} from '@angular/core';
-import {GasToxicity, Tank} from 'scuba-physics';
 import {faSlidersH} from '@fortawesome/free-solid-svg-icons';
 import {ProfileComparatorService} from '../../../shared/profileComparatorService';
 import {IConsumedMix, StandardGases} from 'scuba-physics';
@@ -19,14 +18,6 @@ export class GasConsumedDifferenceComponent {
     public icon = faSlidersH;
 
     constructor(public profileComparatorService: ProfileComparatorService) {
-    }
-
-    public get tanks(): Tank[] {
-        return this.profileComparatorService.profileA.tanksService.tankData;
-    }
-
-    public get toxicity(): GasToxicity {
-        return this.profileComparatorService.profileA.optionsService.toxicity;
     }
 
     public get getMixedTanks(): IMixedTanksForComparison[] {
--- projects/planner/src/app/diff/gasconsumed/diff-gas-consumed/tank-chart/diff-gas-consumed-tank-chart.component.ts
+++ projects/planner/src/app/diff/gasconsumed/diff-gas-consumed/tank-chart/diff-gas-consumed-tank-chart.component.ts
@@ -1,35 +1,100 @@
-import {Component, Input} from '@angular/core';
-import {DecimalPipe, NgIf} from '@angular/common';
-import {GasToxicity, Tank} from 'scuba-physics';
-import {faSlidersH} from '@fortawesome/free-solid-svg-icons';
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
 import {UnitConversion} from '../../../../shared/UnitConversion';
+import {IConsumedMix, StandardGases} from 'scuba-physics';
 
 @Component({
     selector: 'app-diff-gas-consumed-tank-chart',
     templateUrl: './diff-gas-consumed-tank-chart.component.html',
     styleUrl: './diff-gas-consumed-tank-chart.component.scss'
 })
-export class GasConsumedDifferenceTankComponent {
-    @Input()
-    public tank: Tank = Tank.createDefault();
-    @Input()
-    public toxicity = new GasToxicity();
+export class GasConsumedDifferenceTankComponent implements OnChanges{
+    @Input({required: true})
+    public profileACombinedGas: IConsumedMix = {
+            gas: StandardGases.air.copy(),
+            total: 0,
+            consumed: 0,
+            reserve: 0
+        };
+    @Input({required: true})
+    public profileBCombinedGas: IConsumedMix = {
+            gas: StandardGases.air.copy(),
+            total: 0,
+            consumed: 0,
+            reserve: 0
+        };
     @Input()
     public collapsed = false;
 
+    private profileAGasRemaining = this.profileACombinedGas.total - this.profileACombinedGas.consumed;
+    private profileBGasRemaining = this.profileBCombinedGas.total - this.profileBCombinedGas.consumed;
+
     constructor(public units: UnitConversion) { }
 
-    // for the charts we don't need to convert the value
-    // since they are used as percentage
-    public get endPressure(): number {
-        return this.units.fromBar(this.tank.endPressure);
+    public get gasName(): string {
+        return this.profileACombinedGas.gas.name;
+    }
+    public get gasTotalProfileA(): number {
+        return this.units.fromLiter(this.profileACombinedGas.total);
     }
 
-    public get startPressure(): number {
-        return this.units.fromBar(this.tank.startPressure);
+    public get gasTotalProfileB(): number {
+        return this.units.fromLiter(this.profileBCombinedGas.total);
     }
 
-    public get reserve(): number {
-        return this.units.fromBar(this.tank.reserve);
+    public get gasRemainingProfileA(): number {
+        return this.units.fromLiter(this.profileAGasRemaining);
     }
+
+    public get gasRemainingProfileB(): number {
+        return this.units.fromLiter(this.profileBGasRemaining);
+    }
+
+    public get gasReserveProfileA(): number {
+        return this.units.fromLiter(this.profileACombinedGas.reserve);
+    }
+
+    public get gasReserveProfileB(): number {
+        return this.units.fromLiter(this.profileBCombinedGas.reserve);
+    }
+
+    public get gasRemainingDifference(): number {
+        return this.units.fromLiter(this.profileAGasRemaining - this.profileBGasRemaining);
+    }
+    public get gasReserveDifference(): number {
+        return this.units.fromLiter(this.profileACombinedGas.reserve - this.profileBCombinedGas.reserve);
+    }
+
+    public get gasRemainingPercentageDifference(): number {
+        const totalGasRemaining = this.profileAGasRemaining + this.profileBGasRemaining;
+
+        if (totalGasRemaining === 0) {
+            return 0;
+        }
+
+        const profileAPercentage = this.profileAGasRemaining / totalGasRemaining;
+        const profileBPercentage = this.profileBGasRemaining / totalGasRemaining;
+        return Math.abs(profileAPercentage - profileBPercentage) * 50;
+    }
+
+    public get gasReservePercentageDifference(): number {
+        const totalReserve = this.profileACombinedGas.reserve + this.profileBCombinedGas.reserve;
+
+        if (totalReserve === 0) {
+            return 0;
+        }
+
+        const profileAPercentage = this.profileACombinedGas.reserve / totalReserve;
+        const profileBPercentage = this.profileBCombinedGas.reserve / totalReserve;
+        return Math.abs(profileAPercentage - profileBPercentage) * 50;
+    }
+
+
+    ngOnChanges(changes: SimpleChanges) {
+        if (changes.profileACombinedGas || changes.profileBCombinedGas) {
+            this.profileAGasRemaining = this.profileACombinedGas.total - this.profileACombinedGas.consumed;
+            this.profileBGasRemaining = this.profileBCombinedGas.total - this.profileBCombinedGas.consumed;
+        }
+    }
+
+    protected readonly Math = Math;
 }

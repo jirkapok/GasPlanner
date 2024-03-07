c5dc69d001acc0f4ebbbf84e96465a7fe0cc2880
Jiri Pokorny <j.pok@post.cz>
2024-03-08 00:56:57 +0100

    Fixed calculation of emergency ascent.

--- projects/scuba-physics/src/lib/PlanFactory.ts
+++ projects/scuba-physics/src/lib/PlanFactory.ts
@@ -42,31 +42,53 @@ export class PlanFactory {
         return Precision.ceil(estimate);
     }
 
-    public static emergencyAscent(segments: Segment[], options: Options, tanks: Tank[]): Segment[] {
-        const profile = Segments.fromCollection(segments);
-        const deepestPart = profile.deepestPart();
-        const deepestProfile = Segments.fromCollection(deepestPart);
+    public static emergencyAscent(originalPlan: Segment[], options: Options, tanks: Tank[]): Segment[] {
+        const profile = Segments.fromCollection(originalPlan);
+        const emergencySegments = profile.deepestPart();
+        const issueSegmentIndex = emergencySegments.length;
+
+        // 1. add after the deepest part the solving segment - because it may affect the calculated decompression
+        PlanFactory.addSolvingSegment(emergencySegments, options.problemSolvingDuration);
+
+        // 2. add any non deepest segments with tank (i.e. user defined segments)
+        // - this covers overhead environment, where we need to follow user defined ascent
+        PlanFactory.addUserDefinedAscent(emergencySegments, originalPlan, issueSegmentIndex);
+
+        // 3. calculate real ascent and return all segments after the deepest part
+        const emergencyPlan = Segments.fromCollection(emergencySegments);
         const gases = Gases.fromTanks(tanks);
         const algorithm = new BuhlmannAlgorithm();
-        const parameters = AlgorithmParams.forMultilevelDive(deepestProfile, gases, options);
-        const emergencyProfile = algorithm.decompression(parameters);
-        const emergencySegments = emergencyProfile.segments;
-        const ascent = emergencySegments.slice(deepestPart.length, emergencySegments.length);
-        PlanFactory.addSolvingSegment(ascent, options.problemSolvingDuration);
+        const parameters = AlgorithmParams.forMultilevelDive(emergencyPlan, gases, options);
+        const calculatedProfile = algorithm.decompression(parameters);
+        const emergencyProfile = calculatedProfile.segments;
+        const ascent = emergencyProfile.slice(issueSegmentIndex, emergencyProfile.length);
         return ascent;
     }
 
-
-    // in case of user defined gas switch without stay at depth (in ascent segment), we prolong the duration at depth
-    private static addSolvingSegment(ascent: Segment[], problemSolvingDuration: number): void {
-        // all segments are user defined
-        if (ascent.length === 0) {
+    // in case of user defined gas switch without stay at depth (in ascent segment),
+    // we prolong the duration at depth, but with current gas.
+    private static addSolvingSegment(plan: Segment[], problemSolvingDuration: number): void {
+        // no depth defined
+        if (plan.length === 0) {
             return;
         }
 
         const solvingDuration = problemSolvingDuration * Time.oneMinute;
-        const ascentDepth = ascent[0].startDepth;
-        const problemSolving = new Segment(ascentDepth, ascentDepth, ascent[0].gas, solvingDuration);
-        ascent.unshift(problemSolving);
+        const last = plan[plan.length - 1];
+        const ascentDepth = last.endDepth;
+        const problemSolving = new Segment(ascentDepth, ascentDepth, last.gas, solvingDuration);
+        plan.push(problemSolving);
+    }
+
+    // add only segments with tank, i.e. user defined
+    private static addUserDefinedAscent(emergencySegments: Segment[], originalPlan: Segment[], issueSegmentIndex: number): void {
+        for (let index = issueSegmentIndex; index < originalPlan.length; index++) {
+            const userSegment = originalPlan[index];
+            if(!userSegment.tank) {
+                return;
+            }
+
+            emergencySegments.push(userSegment);
+        }
     }
 }

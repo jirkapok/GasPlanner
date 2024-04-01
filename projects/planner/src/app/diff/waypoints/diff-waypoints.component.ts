849092a8d7490611b66053eba9638fbdeb7ad8d4
Jiri Pokorny <j.pok@post.cz>
2024-04-01 09:51:02 +0200

    Added selection to diff table

--- projects/planner/src/app/diff/waypoints/diff-waypoints.component.ts
+++ projects/planner/src/app/diff/waypoints/diff-waypoints.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { faTasks } from '@fortawesome/free-solid-svg-icons';
 import { UnitConversion } from '../../shared/UnitConversion';
 import { ProfileComparatorService } from '../../shared/diff/profileComparatorService';
+import { SelectedDiffWaypoint } from '../../shared/diff/selected-diff-waypoint.service';
+import { ComparedWaypoint } from '../../shared/diff/ComparedWaypoint';
 
 @Component({
     selector: 'app-diff-waypoints',
@@ -12,6 +14,11 @@ export class WaypointsDifferenceComponent {
     public tasks = faTasks;
 
     constructor(public units: UnitConversion,
-        public profileDiff: ProfileComparatorService) {
+        public profileDiff: ProfileComparatorService,
+        private diffSelection: SelectedDiffWaypoint) {
+    }
+
+    public highlightRow(point: ComparedWaypoint | undefined): void {
+        this.diffSelection.selected = point;
     }
 }

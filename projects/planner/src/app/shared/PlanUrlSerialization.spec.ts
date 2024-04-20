f992b431a8aae51e40d388b1cc52bb079ea35e0f
Jiri Pokorny <j.pok@post.cz>
2024-04-20 19:39:13 +0200

    Implemented tests for loading view switch

--- projects/planner/src/app/shared/PlanUrlSerialization.spec.ts
+++ projects/planner/src/app/shared/PlanUrlSerialization.spec.ts
@@ -40,7 +40,7 @@ describe('Url Serialization', () => {
     const irrelevantFactory = new WorkersFactoryCommon();
 
     // because we need custom instances to compare
-    const createSut = (imperial = false): TestSut => {
+    const createSimpleSut = (imperial = false): TestSut => {
         const units = new UnitConversion();
         const dispatcher = new ReloadDispatcher();
         units.imperialUnits = imperial;
@@ -54,8 +54,15 @@ describe('Url Serialization', () => {
         return new TestSut(schedules, planner, viewSwitch, units,  urlSerialization);
     };
 
-    const createCustomSut = () => {
-        const created = createSut();
+    const createSimplePlan = () => {
+        const simpleSut = createSimpleSut();
+        simpleSut.tanksService.tanks[0].size = 18;
+        simpleSut.planner.calculate(1);
+        return simpleSut;
+    };
+
+    const createComplexSut = () => {
+        const created = createSimpleSut();
         created.viewSwitch.isComplex = true;
         created.tanksService.addTank();
         created.depths.addSegment();
@@ -64,20 +71,24 @@ describe('Url Serialization', () => {
     };
 
     /** in metric */
-    let sut: TestSut;
-    let customizedUrl: string;
+    let complexSut: TestSut;
+    let simpleViewUrl: string;
+    let complexViewUrl: string;
 
     beforeEach(() => {
-        sut = createCustomSut();
-        customizedUrl = sut.urlSerialization.toUrl();
+        complexSut = createComplexSut();
+        complexViewUrl = complexSut.urlSerialization.toUrl();
+
+        const simplePlan = createSimplePlan();
+        simpleViewUrl = simplePlan.urlSerialization.toUrl();
     });
 
-    const expectSelectedEquals = (current: TestSut): void => {
+    const expectSelectedEquals = (current: TestSut, expected: TestSut): void => {
         const toExpect = {
-            plan: sut.depths.segments,
-            tanks: sut.tanksService.tankData,
-            diver: sut.options.getDiver(),
-            options: sut.options.getOptions(),
+            plan: expected.depths.segments,
+            tanks: expected.tanksService.tankData,
+            diver: expected.options.getDiver(),
+            options: expected.options.getOptions(),
         };
 
         const toCompare = {
@@ -91,53 +102,36 @@ describe('Url Serialization', () => {
     };
 
     it('Generates valid url characters', () => {
-        const isValid = /[-a-zA-Z0-9@:%_+.~#&//=]*/g.test(customizedUrl);
+        const isValid = /[-a-zA-Z0-9@:%_+.~#&//=]*/g.test(complexViewUrl);
         expect(isValid).toBeTruthy();
     });
 
     it('Generates url of selected dive for multiple dives', () => {
-        sut.schedules.add();
-        sut.schedules.selected = sut.schedules.dives[0];
-        const url = sut.urlSerialization.toUrl();
-        const urlExpected = sut.urlSerialization.toUrlFor(sut.schedules.dives[0].id);
+        complexSut.schedules.add();
+        complexSut.schedules.selected = complexSut.schedules.dives[0];
+        const url = complexSut.urlSerialization.toUrl();
+        const urlExpected = complexSut.urlSerialization.toUrlFor(complexSut.schedules.dives[0].id);
         expect(urlExpected).toEqual(url);
     });
 
     it('Serialize application options', () => {
-        sut.viewSwitch.isComplex = true;
-        sut.units.imperialUnits = true;
-        const url = sut.urlSerialization.toUrl();
+        complexSut.viewSwitch.isComplex = true;
+        complexSut.units.imperialUnits = true;
+        const url = complexSut.urlSerialization.toUrl();
         expect(url).toContain('ao=1,1');
     });
 
-    it('Serialize and deserialize complex plan',() => {
-        const current = createSut();
-        current.urlSerialization.fromUrl(customizedUrl);
-        current.planner.calculate(2);
-        expectSelectedEquals(current);
-    });
-
-    it('Serialize and deserialize simple plan', () => {
-        const simpleSut = createSut();
-        simpleSut.tanksService.tanks[0].size = 18;
-        simpleSut.planner.calculate(1);
-        const urlParams = simpleSut.urlSerialization.toUrl();
-        sut.urlSerialization.fromUrl(urlParams);
-        sut.planner.calculate(2);
-        expectSelectedEquals(simpleSut);
-    });
-
     it('Decodes url for facebook link', () => {
-        const encodedParams = encodeURIComponent(customizedUrl);
-        const current = createSut();
+        const encodedParams = encodeURIComponent(complexViewUrl);
+        const current = createSimpleSut();
         current.urlSerialization.fromUrl(encodedParams);
         current.schedules.remove(current.schedules.dives[0]); // because the dive was added
         current.planner.calculate(1);
-        expectSelectedEquals(current);
+        expectSelectedEquals(current, complexSut);
     });
 
     it('Complex plan in imperial units still generates url below 2048 characters', () => {
-        const current = createSut(true);
+        const current = createSimpleSut(true);
         // tests the limits of url serialization
         // long enough with precise imperial values, still bellow 2k characters
         // consider rounding the workPressure to 3 digits to shorten,
@@ -151,11 +145,49 @@ describe('Url Serialization', () => {
         expect(result.length).toBeLessThan(2048);
     });
 
-    describe('Restore switching units', () => {
+    describe('Complex vs. Simple view', () => {
+        it('Load complex plan to simple view',() => {
+            const current = createSimpleSut();
+            current.urlSerialization.fromUrl(complexViewUrl);
+            current.planner.calculate(2);
+            expectSelectedEquals(current, complexSut);
+            expect(current.viewSwitch.isComplex).toBeTruthy();
+        });
+
+        it('Load simple plan to simple view', () => {
+            const current = createSimpleSut();
+            current.urlSerialization.fromUrl(simpleViewUrl);
+            current.planner.calculate(2);
+            const simpleSut = createSimplePlan();
+            expectSelectedEquals(current, simpleSut);
+            expect(current.viewSwitch.isComplex).toBeFalsy();
+        });
+
+        it('Load simple plan to complex view', () => {
+            const simpleSut = createSimplePlan();
+            const urlParams = simpleSut.urlSerialization.toUrl();
+            complexSut.urlSerialization.fromUrl(urlParams);
+            complexSut.planner.calculate(2);
+            expectSelectedEquals(simpleSut, complexSut);
+            expect(complexSut.viewSwitch.isComplex).toBeTruthy();
+        });
+
+        it('Restore complex to complex', () => {
+            const currentComplex = createSimpleSut();
+            currentComplex.viewSwitch.isComplex = true;
+            currentComplex.urlSerialization.fromUrl(complexViewUrl);
+            currentComplex.planner.calculate(2);
+            expectSelectedEquals(currentComplex, complexSut);
+            expect(currentComplex.viewSwitch.isComplex).toBeTruthy();
+        });
+    });
+
+    // TODO fix restore url from imperial units
+    describe('Switching units', () => {
         xit('From metric units', () => {
-            sut.tanksService.firstTank.size = 24;
-            const url = sut.urlSerialization.toUrl();
-            const current = createSut(true);
+            complexSut.tanksService.firstTank.size = 24;
+            const url = complexSut.urlSerialization.toUrl();
+            const current = createSimpleSut(true);
             current.tanksService.firstTank.workingPressure = 250;
             current.urlSerialization.fromUrl(url);
 
@@ -166,33 +198,33 @@ describe('Url Serialization', () => {
         });
 
         xit('From imperial units', () => {
-            const current = createSut(true);
+            const current = createSimpleSut(true);
             current.tanksService.firstTank.size = 240;
             const url = current.urlSerialization.toUrl();
-            sut.urlSerialization.fromUrl(url);
+            complexSut.urlSerialization.fromUrl(url);
 
-            const firstTank = sut.tanksService.firstTank;
+            const firstTank = complexSut.tanksService.firstTank;
             expect(firstTank.workingPressure).toBeCloseTo(3442, 3);
             expect(firstTank.size).toBeCloseTo(240, 3);
         });
 
         xit('Switching units corrects out of metric range values', () => {
-            const current = createSut(true);
+            const current = createSimpleSut(true);
             // still valid value in imperial, but not in metric
             current.tanksService.firstTank.size = 1;
             const url = current.urlSerialization.toUrl();
-            sut.urlSerialization.fromUrl(url);
+            complexSut.urlSerialization.fromUrl(url);
 
-            const firstTank = sut.tanksService.firstTank;
+            const firstTank = complexSut.tanksService.firstTank;
             expect(firstTank.size).toBeCloseTo(1, 3);
         });
     });
 
     describe('Skips loading', () => {
         const assertImported = (urlParams: string): void => {
-            const current = createCustomSut();
+            const current = createComplexSut();
             current.urlSerialization.fromUrl(urlParams);
-            expectSelectedEquals(current);
+            expectSelectedEquals(current, complexSut);
         };
 
         it('Invalid url values', () => {
@@ -211,9 +243,9 @@ describe('Url Serialization', () => {
         it('Invalid working pressure in imperial', () => {
             const urlParams = 't=1-15-220-210-0.209-0,2-11.1-0-200-0.209-0&de=0-30-102-1,30-30-618-1,30-30-600-1&' +
                 'di=20&o=0,9,6,3,3,18,2,0.85,0.4,3,1.6,30,1.4,10,1,1,0,2,1&ao=1,1';
-            const current = createCustomSut();
+            const current = createComplexSut();
             current.urlSerialization.fromUrl(urlParams);
-            expectSelectedEquals(current);
+            expectSelectedEquals(current, complexSut);
         });
 
         it('Empty string', () => {

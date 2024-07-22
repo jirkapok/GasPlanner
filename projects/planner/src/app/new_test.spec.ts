70b1ab62992f47a96f1eeaa92dc3b7f38ade3dfb
josefbogar <161020900+josefbogar@users.noreply.github.com>
2024-07-22 22:32:21 +0200

    poslední commit 22.7

--- projects/planner/src/app/new_test.spec.ts
+++ projects/planner/src/app/new_test.spec.ts
@@ -1,5 +1,6 @@
+/* eslint-disable jasmine/no-focused-tests */
 describe('First Jasmine test', () => {
-    it('Should correctly add two numbers', () => {
+    fit('Should correctly add two numbers', () => {
         console.log('Running Should Correctly add two numbers');
         const result = 1 + 2;
         expect(result).toBe(3);

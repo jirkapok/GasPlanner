606cf62113b1f18281f404cd3ddba69473543215
josefbogar <161020900+josefbogar@users.noreply.github.com>
2024-07-24 22:24:54 +0200

    delete new_test.spec.ts

--- projects/planner/src/app/new_test.spec.ts
+++
@@ -1,7 +0,0 @@
-describe('First Jasmine test', () => {
-    it('Should Correctly add two numbers', () => {
-        console.log('Running First Jasmine Test');
-        const result = 1 + 2;
-        expect(result).toBe(3);
-    });
-});

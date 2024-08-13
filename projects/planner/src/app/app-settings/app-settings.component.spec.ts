2c86ac80e94170929282374144a2e227ade8cec1
Jiri Pokorny <j.pok@post.cz>
2024-08-14 01:24:30 +0200

    Updated test cases

--- projects/planner/src/app/app-settings/app-settings.component.spec.ts
+++ projects/planner/src/app/app-settings/app-settings.component.spec.ts
@@ -84,8 +84,7 @@ describe('App settings component', () => {
 
 // TODO AppSettings:
 // * After changing units, also the range values need to be reloaded
-// * Add to application settings serialization/persistence
-// * Add save and load last from state
+// * Add save and load last form state. Do we want this? Or together with reset to default?
 // * Apply to algorithm
 // * add to normalization service
 
@@ -98,8 +97,15 @@ describe('App settings component', () => {
 //   * Values are reloaded after switch to and from imperial units and click Use button
 //   * Values are Saved to PreferencesStore after clicking Use button
 //   * Values are loaded from PreferencesStore after component initialization
-// * PersistenceService: Save and load AppSettings from store
+// * PersistenceService:
+//   * Save and load AppSettings from store (density)
+//   * Density not saved yet, default value is loaded
+// * LoadFromUrl:
+//   * Loads current density (no change)
+//   * Loads and saves stressRmv and rmv
+//   * Loads default stressRmv, if not present in URL
 // * ProfileEvents: Generates correct events based on maxDensity
 // * NormalizationService: rounds values to correct range of settings when switching units
+//   * What are the values to test?
 // * Scheduler: Change of maxDensity triggers schedule calculation
 // * Diver.component.ts: Add tests for rmv and stressRmv save and load

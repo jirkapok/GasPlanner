ccfd798d8d607135185f2b3cfe85ed264571c3cc
josefbogar <161020900+josefbogar@users.noreply.github.com>
2024-08-16 01:40:03 +0200

    create new branch newTestDiver

--- projects/scuba-physics/src/lib/Diver.spec.ts
+++ projects/scuba-physics/src/lib/Diver.spec.ts
@@ -2,28 +2,23 @@ import { Diver} from 'scuba-physics';
 import {Tank} from 'scuba-physics';
 
 describe('Diver', () => {
-    // eslint-disable-next-line jasmine/no-focused-tests
-    fit('Constructor', () => {
+
+    it('Diver.rmv', () => {
         const diver = new Diver(20, 25);
         expect(diver.rmv).toBe(20);
     });
 
-    describe('Diver StressRmv', () => {
-        // eslint-disable-next-line jasmine/no-focused-tests
-        fit('Constructor', () => {
-            const diver = new Diver(20, 30);
-            expect(diver.stressRmv).toBe(30);
-        });
+    it('Diver.stressRmv', () => {
+        const diver = new Diver(20, 30);
+        expect(diver.stressRmv).toBe(30);
+    });
 
-        // eslint-disable-next-line jasmine/no-suite-dupes
-        describe('Diver', () => {
-            // eslint-disable-next-line jasmine/no-focused-tests
-            fit('should correctly calculate gas SAC based on tank size', () => {
-                const diver = new Diver (20, 30);
-                const tank = new Tank(100, 100, 20);
-                tank.size = 100;
-                expect(diver.gasSac(tank)).toBe(0.2);
-            });
-        });
+    it('should correctly calculate gas SAC based on tank size', () => {
+        const diver = new Diver (20, 30);
+        const tank = new Tank(100, 100, 20);
+        tank.size = 100;
+        expect(diver.gasSac(tank)).toBe(0.2);
     });
 });
+
+

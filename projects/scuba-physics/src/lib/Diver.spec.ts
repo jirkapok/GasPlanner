eab88c3507971cb88ba2144e25378f0b3c6e242d
josefbogar <161020900+josefbogar@users.noreply.github.com>
2024-08-15 22:59:58 +0200

    second test Diver

--- projects/scuba-physics/src/lib/Diver.spec.ts
+++ projects/scuba-physics/src/lib/Diver.spec.ts
@@ -1,4 +1,5 @@
-import { Diver } from 'scuba-physics';
+import { Diver} from 'scuba-physics';
+import {Tank} from 'scuba-physics';
 
 describe('Diver', () => {
     // eslint-disable-next-line jasmine/no-focused-tests
@@ -13,6 +14,16 @@ describe('Diver', () => {
             const diver = new Diver(20, 30);
             expect(diver.stressRmv).toBe(30);
         });
+
+        // eslint-disable-next-line jasmine/no-suite-dupes
+        describe('Diver', () => {
+            // eslint-disable-next-line jasmine/no-focused-tests
+            fit('should correctly calculate gas SAC based on tank size', () => {
+                const diver = new Diver (20, 30);
+                const tank = new Tank(100, 100, 20);
+                tank.size = 100;
+                expect(diver.gasSac(tank)).toBe(0.2);
+            });
+        });
     });
 });
-

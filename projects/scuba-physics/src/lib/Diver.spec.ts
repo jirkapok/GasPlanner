3937ce1f142ee41419949b65d00c2597ca1afeb6
Jiri Pokorny <j.pok@post.cz>
2024-08-25 13:48:20 +0200

    Code cleanup in Diver spec

--- projects/scuba-physics/src/lib/Diver.spec.ts
+++ projects/scuba-physics/src/lib/Diver.spec.ts
@@ -2,18 +2,27 @@ import { Diver} from './Diver';
 import { Tank} from './Tanks';
 
 describe('Diver', () => {
+    describe('Create', () => {
+        it('Assigns given values', () => {
+            const diver = new Diver(25, 26);
+            expect(diver.rmv).toBe(25);
+            expect(diver.stressRmv).toBe(26);
+        });
 
-    it('Diver.rmv', () => {
-        const diver = new Diver(20, 25);
-        expect(diver.rmv).toBe(20);
+        it('Assigns default rmv and stressRmv', () => {
+            const diver = new Diver();
+            expect(diver.rmv).toBe(20);
+            expect(diver.stressRmv).toBe(30);
+        });
+
+        it('Assigns default stress rmv only', () => {
+            const diver = new Diver(18);
+            expect(diver.rmv).toBe(18);
+            expect(diver.stressRmv).toBe(27);
+        });
     });
 
-    it('Diver.stressRmv', () => {
-        const diver = new Diver(20, 30);
-        expect(diver.stressRmv).toBe(30);
-    });
-
-    it('should correctly change rmv and stressRmv', () => {
+    it('applies rmv and stressRmv', () => {
         const diver = new Diver(20, 30);
         diver.rmv = 25;
         diver.stressRmv = 35;
@@ -21,57 +30,24 @@ describe('Diver', () => {
         expect(diver.stressRmv).toBe(35);
     });
 
-    it('should correctly assign default rmv and stressRmv if undefined', () => {
-        const diver = new Diver(undefined, undefined);
-        expect(diver.rmv).toBe(Diver.defaultSac);
-        expect(diver.stressRmv).toBe(Diver.defaultSac * 1.5);
+    it('calculates teamStressRmv from stressRmv', () =>{
+        const diver = new Diver(20, 24);
+        expect(diver.teamStressRmv).toBe(48);
     });
 
-    it('constructor with default values', () => {
-        const diver = new Diver();
-        expect(diver.rmv).toBe(Diver.defaultSac);
-        expect(diver.stressRmv).toBe(Diver.defaultSac * 1.5);
+    it('calculates gas SAC based on tank size', () => {
+        const diver = new Diver(12);
+        const tank = new Tank(10, 200, 21);
+        expect(diver.gasSac(tank)).toBe(1.2);
     });
 
-    it('constructor with rmv defined only ', () => {
-        const diver = new Diver(18);
-        expect(diver.rmv).toBe(18);
-        expect(diver.stressRmv).toBe(18 * 1.5);
-    });
-
-    it('constructor with defined values ', () => {
-        const diver = new Diver(30,50);
-        expect(diver.rmv).toBe(30);
-        expect(diver.stressRmv).toBe(50);
-    });
-
-    it('should correctly calculate teamStressRmv', () =>{
-        const stressRmv = 30;
-        const diver = new Diver(20, stressRmv);
-        expect(diver.teamStressRmv).toBe(60);
-    });
-
-    it('should correctly calculate gas SAC based on tank size', () => {
-        const diver = new Diver (20, 30);
-        const tank = new Tank(100, 100, 20);
-        tank.size = 100;
-        expect(diver.gasSac(tank)).toBe(0.2);
-    });
-
-    it('should correctly calculate gas SAC based on instance properties', () => {
-        const diver = new Diver();
-        const tank = new Tank(100, 100, 20);
-        expect(diver.gasSac(tank)).toBe(0.2);
-    });
-
-    it('should correctly pass values from Diver instance', () => {
-        const diver1 = new Diver(30, 45);
+    it('loadFrom should correctly copy values', () => {
+        const diver1 = new Diver(22, 45);
         const diver2 = new Diver();
         diver2.loadFrom(diver1);
-        expect(diver2.rmv).toBe(30);
+        expect(diver2.rmv).toBe(22);
         expect(diver2.stressRmv).toBe(45);
     });
-
 });
 
 

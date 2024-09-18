13a9f91e3673a908d4fd72168456cb56cb6d1fe0
Jiri Pokorny <j.pok@post.cz>
2024-09-19 00:55:37 +0200

    Added validation to Gasblend pricing.

--- projects/scuba-physics/src/lib/blendPricing.ts
+++ projects/scuba-physics/src/lib/blendPricing.ts
@@ -31,6 +31,7 @@ export class BlendPricing {
      * @param costs not null settings for gas prices and amounts to add.
      */
     public static price(costs: BlendCosts): BlendPrice {
+        BlendPricing.validateCosts(costs);
         const o2Price = costs.addO2 * costs.o2UnitPrice;
         const hePrice = costs.addHe * costs.heUnitPrice;
         const topMixPrice = costs.addTop * costs.topMixUnitPrice;
@@ -41,5 +42,26 @@ export class BlendPricing {
             topMixPrice: topMixPrice,
             totalPrice: o2Price + hePrice + topMixPrice
         };
+    }
+
+    private static validateCosts(costs: BlendCosts) {
+        if (costs.addO2 < 0) {
+            throw new Error('addO2 must be positive number');
+        }
+        if (costs.o2UnitPrice < 0) {
+            throw new Error('o2UnitPrice must be positive number');
+        }
+        if (costs.addHe < 0) {
+            throw new Error('addHe must be positive number');
+        }
+        if (costs.heUnitPrice < 0) {
+            throw new Error('heUnitPrice must be positive number');
+        }
+        if (costs.addTop < 0) {
+            throw new Error('addTop must be positive number');
+        }
+        if (costs.topMixUnitPrice < 0) {
+            throw new Error('topMixUnitPrice must be positive number');
+        }
     }
 }

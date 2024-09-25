239e3c01f05eaac49a02a0444a214da53e4b37e5
josefbogar <161020900+josefbogar@users.noreply.github.com>
2024-09-25 20:22:45 +0200

    add tests for negative numbers

--- projects/scuba-physics/src/lib/blendPricing.spec.ts
+++ projects/scuba-physics/src/lib/blendPricing.spec.ts
@@ -1,7 +1,7 @@
 import { BlendPricing, BlendCosts, BlendPrice } from './blendPricing';
 
 describe('BlendPricing', () => {
-    /** we will not test negative numbers */
+
     it('should calculate prices according declared amount and price of gas', () => {
 
         const costs: BlendCosts = {
@@ -24,4 +24,85 @@ describe('BlendPricing', () => {
 
         expect(result).toEqual(expected);
     });
+
+    describe('Negative costs validation', () => {
+        it('should throw an error for negative addO2', () => {
+            const negativeCosts: BlendCosts = {
+                addO2: -1,
+                o2UnitPrice: 5,
+                addHe: 3,
+                heUnitPrice: 8,
+                addTop: 4,
+                topMixUnitPrice: 10
+            };
+
+            expect(() => BlendPricing.price(negativeCosts)).toThrowError('addO2 must be positive number');
+        });
+
+        it('should throw an error for negative o2UnitPrice', () => {
+            const negativeCosts: BlendCosts = {
+                addO2: 2,
+                o2UnitPrice: -5,
+                addHe: 3,
+                heUnitPrice: 8,
+                addTop: 4,
+                topMixUnitPrice: 10
+            };
+
+            expect(() => BlendPricing.price(negativeCosts)).toThrowError('o2UnitPrice must be positive number');
+        });
+
+        it('should throw an error for negative addHe', () => {
+            const negativeCosts: BlendCosts = {
+                addO2: 2,
+                o2UnitPrice: 5,
+                addHe: -3,
+                heUnitPrice: 8,
+                addTop: 4,
+                topMixUnitPrice: 10
+            };
+
+            expect(() => BlendPricing.price(negativeCosts)).toThrowError('addHe must be positive number');
+        });
+
+        it('should throw an error for negative heUnitPrice', () => {
+            const negativeCosts: BlendCosts = {
+                addO2: 2,
+                o2UnitPrice: 5,
+                addHe: 3,
+                heUnitPrice: -8,
+                addTop: 4,
+                topMixUnitPrice: 10
+            };
+
+            expect(() => BlendPricing.price(negativeCosts)).toThrowError('heUnitPrice must be positive number');
+        });
+
+        it('should throw an error for negative addTop', () => {
+            const negativeCosts: BlendCosts = {
+                addO2: 2,
+                o2UnitPrice: 5,
+                addHe: 3,
+                heUnitPrice: 8,
+                addTop: -4,
+                topMixUnitPrice: 10
+            };
+
+            expect(() => BlendPricing.price(negativeCosts)).toThrowError('addTop must be positive number');
+        });
+
+        it('should throw an error for negative topMixUnitPrice', () => {
+            const negativeCosts: BlendCosts = {
+                addO2: 2,
+                o2UnitPrice: 5,
+                addHe: 3,
+                heUnitPrice: 8,
+                addTop: 4,
+                topMixUnitPrice: -10
+            };
+
+            expect(() => BlendPricing.price(negativeCosts)).toThrowError('topMixUnitPrice must be positive number');
+        });
+    });
 });
+

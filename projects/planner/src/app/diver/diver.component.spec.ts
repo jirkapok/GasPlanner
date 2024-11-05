bb9a867376acb338c585f285df629af2b04a162f
josefbogar <161020900+josefbogar@users.noreply.github.com>
2024-11-05 22:54:37 +0100

    add diver.component tests

--- projects/planner/src/app/diver/diver.component.spec.ts
+++ projects/planner/src/app/diver/diver.component.spec.ts
@@ -1,47 +1,78 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
-import { DiverComponent } from './diver.component';
+import {NonNullableFormBuilder, FormGroup}
+import {DiverComponent} from'./diver.component';
+import { DecimalPipe } from '@angular/common';
 import { UnitConversion } from '../shared/UnitConversion';
 import { ValidatorGroups } from '../shared/ValidatorGroups';
 import { InputControls } from '../shared/inputcontrols';
 import { DiverOptions } from '../shared/models';
-import { By } from '@angular/platform-browser';
-import { DecimalPipe } from '@angular/common';
-
-class DiverPage {
-    constructor(private fixture: ComponentFixture<DiverComponent>) {}
-
-    public get stressRmvInput(): HTMLInputElement {
-        return this.fixture.debugElement.query(By.css('#stressRmv')).nativeElement as HTMLInputElement;
-    }
-}
 
 describe('DiverComponent', () => {
     let component: DiverComponent;
-    let fixture: ComponentFixture<DiverComponent>;
-    let diverPage: DiverPage;
-
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            declarations: [DiverComponent],
-            imports: [ReactiveFormsModule],
-            providers: [UnitConversion, ValidatorGroups, InputControls, DecimalPipe]
-        }).compileComponents();
-    });
+    let unitConversion: UnitConversion;
 
     beforeEach(() => {
-        fixture = TestBed.createComponent(DiverComponent);
-        component = fixture.componentInstance;
+        const formBuilder = new NonNullableFormBuilder();
+        const decimalPipe = new DecimalPipe('en-US');
+        const inputControls = new InputControls(decimalPipe);
+        unitConversion = new UnitConversion();
+        const validatorGroups = new ValidatorGroups(unitConversion);
+
+
+        component = new DiverComponent(
+            formBuilder,
+            inputControls,
+            validatorGroups,
+            unitConversion
+        );
+
+            component.diverForm = formBuilder.group({
+            rmv: ['20'],
+            stressRmv: ['25']
+        });
+
         component.diver = new DiverOptions();
-        fixture.detectChanges();
-        diverPage = new DiverPage(fixture);
     });
 
-    // eslint-disable-next-line jasmine/no-focused-tests
-    fit('should display stress RMV value in the input field', () => {
-        component.diver.stressRmv = 30;
-        fixture.detectChanges();
+    describe('inputChanged', () => {
+        it('should update rmv and stressRmv values in liters and emit changes', () => {
+            const rmv = 15;
+            const stressRmv = 20;
+            component.diverForm.setValue({ rmv: rmv, stressRmv: stressRmv });
+            let wasEmitted = false;
+            component.changed.subscribe(() => wasEmitted = true);
 
-        expect(diverPage.stressRmvInput.value).toBe('30');
+            component.inputChanged();
+
+            expect(component.diver.rmv).toBe(unitConversion.toLiter(rmv));
+            expect(component.diver.stressRmv).toBe(unitConversion.toLiter(stressRmv));
+            expect(wasEmitted).toBeTrue();
+        });
     });
+
+
+    describe('maxPpO2Changed', () => {
+        it('should change new value of maxPpO2 and emit change', () => {
+            const newPpO2 = 1.2;
+            let wasEmitted = false;
+            component.changed.subscribe(() => wasEmitted = true);
+
+            component.maxPpO2Changed(newPpO2);
+
+
+            expect(component.diver.maxPpO2).toBe(newPpO2);
+            expect(wasEmitted).toBeTrue();
+        });
+    });
+    describe('maxDecoPpO2Changed', () => {
+        it('should change new value of maxDecoPpO2 and emit change', () => {
+            const newDecoPpO2 = 1.4;
+            let wasEmitted = false;
+            component.changed.subscribe(() => wasEmitted = true);
+
+            component.maxDecoPpO2Changed(newDecoPpO2);
+            expect(component.diver.maxDecoPpO2).toBe(newDecoPpO2);
+            expect(wasEmitted).toBeTrue();
+        })
+    })
 });

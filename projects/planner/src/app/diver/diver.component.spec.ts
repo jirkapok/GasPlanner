f21127e55e832ef13f888f621b1fc367862b962d
josefbogar <161020900+josefbogar@users.noreply.github.com>
2024-11-19 14:31:50 +0100

    add change rmvStep test

--- projects/planner/src/app/diver/diver.component.spec.ts
+++ projects/planner/src/app/diver/diver.component.spec.ts
@@ -31,54 +31,58 @@ describe('DiverComponent', () => {
         fixture.detectChanges();
     });
 
-    describe('inputChanged', () => {
-        it('should change rmv and stressRmv values and emit', () => {
-            expect(component.diver.rmv).toBe(20);
-            expect(component.diver.stressRmv).toBe(30);
+    it('should change rmv and stressRmv values and emit', () => {
+        expect(component.diver.rmv).toBe(20);
+        expect(component.diver.stressRmv).toBe(30);
 
-            const newRmv = 15;
-            const newStressRmv = 25;
+        const newRmv = 15;
+        const newStressRmv = 25;
 
-            component.diverForm.setValue({ rmv: newRmv, stressRmv: newStressRmv });
+        component.diverForm.setValue({ rmv: newRmv, stressRmv: newStressRmv });
 
-            let wasEmitted = false;
-            component.changed.subscribe(() => wasEmitted = true);
+        let wasEmitted = false;
+        component.changed.subscribe(() => wasEmitted = true);
 
-            component.inputChanged();
+        component.inputChanged();
 
-            expect(component.diver.rmv).toBe(unitConversion.toLiter(newRmv));
-            expect(component.diver.stressRmv).toBe(unitConversion.toLiter(newStressRmv));
-            expect(wasEmitted).toBeTrue();
-        });
+        expect(component.diver.rmv).toBe(unitConversion.toLiter(newRmv));
+        expect(component.diver.stressRmv).toBe(unitConversion.toLiter(newStressRmv));
+        expect(wasEmitted).toBeTrue();
     });
 
-    describe('maxPpO2Changed', () => {
-        it('should change new value of maxPpO2 and emit', () => {
-            expect(component.diver.maxPpO2).toBe(1.4);
+    it('should change new value of maxPpO2 and emit', () => {
+        expect(component.diver.maxPpO2).toBe(1.4);
 
-            const newPpO2 = 1.2;
-            let wasEmitted = false;
-            component.changed.subscribe(() => wasEmitted = true);
+        const newPpO2 = 1.2;
+        let wasEmitted = false;
+        component.changed.subscribe(() => wasEmitted = true);
 
-            component.maxPpO2Changed(newPpO2);
+        component.maxPpO2Changed(newPpO2);
 
-            expect(component.diver.maxPpO2).toBe(newPpO2);
-            expect(wasEmitted).toBeTrue();
-        });
+        expect(component.diver.maxPpO2).toBe(newPpO2);
+        expect(wasEmitted).toBeTrue();
     });
 
-    describe('maxDecoPpO2Changed', () => {
-        it('should change new value of maxDecoPpO2 and emit', () => {
-            expect(component.diver.maxDecoPpO2).toBe(1.6);
+    it('should change new value of maxDecoPpO2 and emit', () => {
+        expect(component.diver.maxDecoPpO2).toBe(1.6);
 
-            const newDecoPpO2 = 1.4;
-            let wasEmitted = false;
-            component.changed.subscribe(() => wasEmitted = true);
+        const newDecoPpO2 = 1.4;
+        let wasEmitted = false;
+        component.changed.subscribe(() => wasEmitted = true);
 
-            component.maxDecoPpO2Changed(newDecoPpO2);
+        component.maxDecoPpO2Changed(newDecoPpO2);
 
-            expect(component.diver.maxDecoPpO2).toBe(newDecoPpO2);
-            expect(wasEmitted).toBeTrue();
-        });
+        expect(component.diver.maxDecoPpO2).toBe(newDecoPpO2);
+        expect(wasEmitted).toBeTrue();
+    });
+
+    it('should change rmvStep when switching to imperial units', () => {
+        expect(component.rmvStep).toBe(2);
+
+        unitConversion.imperialUnits = true;
+
+        fixture.detectChanges();
+
+        expect(component.rmvStep).toBe(0.1);
     });
 });

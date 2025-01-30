9e437954ace88f88874401dab6691d7d1aaf02c0
josefbogar <161020900+josefbogar@users.noreply.github.com>
2025-01-30 23:01:00 +0100

    title correction

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,6 +3,6 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
     await page.goto('/');
 
-    await expect(page).toHaveTitle(/Gas planner/);
+    await expect(page).toHaveTitle(/Dugong Gas Planner/);
 });
 

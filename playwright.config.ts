f6271e7a67ca36c89f4bdb513e44a1c07dc5d04d
Jiri Pokorny <j.pok@post.cz>
2025-02-04 22:04:21 +0100

    Updated e2e tests config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,19 +1,28 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-    testDir: './tests',
-    timeout: 30 * 1000,
-    expect: {
-        timeout: 5000
-    },
-    use: {
-        baseURL: 'http://localhost:4200',
-        trace: 'on-first-retry',
-    },
-    projects: [
-        {
-            name: 'chromium',
-            use: { ...devices['Desktop Chrome'] },
-        }
-    ],
+  testDir: './e2e',
+  outputDir: './test-results',
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 5000
+  },
+  fullyParallel: true,
+  forbidOnly: !!process.env['CI'],
+  retries: process.env['CI'] ? 2 : 0,
+  workers: process.env['CI'] ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: [
+      ['junit', { outputFile: 'test-results/e2e.xml' }]
+  ],
+  use: {
+      baseURL: process.env['PLAYWRIGHT_TEST_BASE_URL'] ?? 'http://localhost:4200',
+      trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    }
+  ],
 });

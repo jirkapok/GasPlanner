05b1690392bc71dfc2cf8be26d4845a408c54cab
Jiri Pokorny <j.pok@post.cz>
2025-02-04 23:35:15 +0100

    Added e2e dependencies to install script

--- build/installEnvironment.ps1
+++ build/installEnvironment.ps1
@@ -7,11 +7,15 @@ nvm use lts
 npm install -g @angular/cli
 npm install -g typescript
 
+# For E2e tests
+npx playwright install chromium
+
 # Visual Studio Code
 choco install vscode -y
 # Optional extensions
 # code --install-extension ms-vscode.powershell
 # code --install-extension angular.ng-template
+# code --install-extension ms-playwright.playwright
 
 # NPM Packages
 .\build.ps1

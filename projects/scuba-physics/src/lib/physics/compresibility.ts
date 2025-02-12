28ddd7f2db62a72454099be6f8e204ece2a4114f
Jiri Pokorny <j.pok@post.cz>
2025-02-13 00:13:51 +0100

    Added documentation to compressibility

--- projects/scuba-physics/src/lib/physics/compresibility.ts
+++ projects/scuba-physics/src/lib/physics/compresibility.ts
@@ -1,6 +1,7 @@
 import { Gas } from '../gases/Gases';
 
 export class Compressibility {
+    private readonly normalPressure = 1;
     private readonly o2Coefficients = [-7.18092073703e-4, 2.81852572808e-6, -1.50290620492e-9];
     private readonly n2Coefficients = [-2.19260353292e-4, 2.92844845532e-6, -2.07613482075e-9];
     private readonly heCoefficients = [4.87320026468e-4, -8.83632921053e-8, 5.33304543646e-11];
@@ -11,23 +12,38 @@ export class Compressibility {
             coefficients[2] * pressure * pressure * pressure;
     }
 
-    public zFactor(p: number, gas: Gas): number {
+    /**
+     * Calculates compressibility Z-factor for given gas mixture at given pressure.
+     * @param gasPressure Gas pressure in bar
+     * @param gas Not empty gas mixture
+     **/
+    public zFactor(gasPressure: number, gas: Gas): number {
         return (
             1 +
-            gas.fO2 * this.virial(p, this.o2Coefficients) +
-            gas.fHe * this.virial(p, this.heCoefficients) +
-            gas.fN2 * this.virial(p, this.n2Coefficients)
+            gas.fO2 * this.virial(gasPressure, this.o2Coefficients) +
+            gas.fHe * this.virial(gasPressure, this.heCoefficients) +
+            gas.fN2 * this.virial(gasPressure, this.n2Coefficients)
         );
     }
 
+    /**
+     * Calculates normal volume in liters for given gas mixture at 1 bar.
+     * @param gasPressure current gas pressure in bar
+     * @param gas Not empty gas mixture
+     **/
     public normalVolumeFactor(gasPressure: number, gas: Gas): number {
-        return (gasPressure * this.zFactor(1, gas)) / this.zFactor(gasPressure, gas);
+        return (gasPressure * this.zFactor(this.normalPressure, gas)) / this.zFactor(gasPressure, gas);
     }
 
+    /**
+     * Finds current gas pressure for given gas volume.
+     * @param gas Not empty gas mixture
+     * @param volume Gas volume in liters
+     */
     public findPressure(gas: Gas, volume: number): number {
         let foundPressure = volume;
-        while (Math.abs(this.zFactor(1, gas) * foundPressure - this.zFactor(foundPressure, gas) * volume) > 0.000001) {
-            foundPressure = (volume * this.zFactor(foundPressure, gas)) / this.zFactor(1, gas);
+        while (Math.abs(this.zFactor(this.normalPressure, gas) * foundPressure - this.zFactor(foundPressure, gas) * volume) > 0.000001) {
+            foundPressure = (volume * this.zFactor(foundPressure, gas)) / this.zFactor(this.normalPressure, gas);
         }
         return foundPressure;
     }

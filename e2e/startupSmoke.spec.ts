b9547c5c2eb2fb51feaa1189d2b6f5d3062558f7
josefbogar <161020900+josefbogar@users.noreply.github.com>
2025-03-03 22:45:12 +0100

    correction of e2e tests according to comments

--- e2e/startupSmoke.spec.ts
+++ e2e/startupSmoke.spec.ts
@@ -1,7 +1,6 @@
 import { test, expect, Page, type Locator, BrowserContext } from '@playwright/test';
 
 class DiveInfoPage {
-    private readonly titleSelector = '#dive-info-title';
     private readonly timeLabelSelector = '#total-dive-time';
     private readonly timeValueSelector = '#total-dive-time-value';
     private readonly waypointTableSelector = '#dive-waypoints-table';
@@ -10,11 +9,7 @@ class DiveInfoPage {
     constructor(private page: Page) {}
 
     async navigate() {
-        await this.page.goto('http://localhost:4200/');
-    }
-
-    getTitle(): Locator {
-        return this.page.locator(this.titleSelector);
+        await this.page.goto('/');
     }
 
     getTimeLabel(): Locator {
@@ -39,19 +34,12 @@ test.describe('Dive Info Tests', () => {
     let context: BrowserContext;
     let page: Page;
 
-    test.beforeEach(async ({ browser }) => {
+    test('should show total dive time and display six waypoint rows', async ({ browser }) => {
         context = await browser.newContext();
-
         page = await context.newPage();
 
         diveInfoPage = new DiveInfoPage(page);
         await diveInfoPage.navigate();
-    });
-
-    test('should show dive info', async () => {
-        const title = diveInfoPage.getTitle();
-        await expect(title).toBeVisible();
-        await expect(title).toHaveText('Dive info');
 
         const timeLabel = diveInfoPage.getTimeLabel();
         await expect(timeLabel).toBeVisible();
@@ -60,11 +48,6 @@ test.describe('Dive Info Tests', () => {
         const timeValue = diveInfoPage.getTimeValue();
         await expect(timeValue).toBeVisible();
         await expect(timeValue).toHaveText('21:00');
-    });
-
-    test('should display six waypoint rows', async () => {
-        const waypointTable = diveInfoPage.getWaypointTable();
-        await expect(waypointTable).toBeVisible();
 
         const waypointRows = diveInfoPage.getWaypointRows();
         await expect(waypointRows).toHaveCount(6);

8be02e672e044f27b53faa5dfd4725e4b00f43bc
Darkvikis <48491740+Darkvikis@users.noreply.github.com>
2025-04-20 11:31:41 +0200

    Refactor card headers in gas-blender, nitrox, sac, diveinfo, profilechart, and waypoints components to use app-card-header and streamline button integration

--- projects/planner/src/app/card-header/card-header.component.ts
+++ projects/planner/src/app/card-header/card-header.component.ts
@@ -2,7 +2,7 @@ import { Component,Input } from '@angular/core';
 import { MdbModalRef, MdbModalService } from 'mdb-angular-ui-kit/modal';
 import { HelpModalComponent } from '../help/help-modal.component';
 import { FeatureFlags } from 'scuba-physics';
-import { faCircleInfo, faShareFromSquare, faThumbsUp, faFilter, faFire, faSackDollar } from '@fortawesome/free-solid-svg-icons';
+import { faCircleInfo} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
     selector: 'app-card-header',
@@ -13,45 +13,15 @@ import { faCircleInfo, faShareFromSquare, faThumbsUp, faFilter, faFire, faSackDo
 export class CardHeaderComponent {
 
     @Input() public title = 'Untitled';
-    @Input() public showShare = false;
-    @Input() public showFilter = false;
-    @Input() public showHeatMap = false;
-    @Input() public showTogglePricing = false;
-    @Input() public showEmergencyAscent = false;
     @Input() public helpName = 'not-implemented';
     @Input() public headerIcon = faCircleInfo;
 
     public readonly helpIcon = faCircleInfo;
-    public readonly iconShare = faShareFromSquare;
-    public readonly filterIcon = faFilter;
-    public readonly emergencyIcon = faThumbsUp;
-    public readonly heatmapIcon = faFire;
-    public readonly dollarIcon = faSackDollar;
     public integratedHelp = FeatureFlags.Instance.integratedHelp;
 
     private modalRef: MdbModalRef<HelpModalComponent> | null = null;
 
     constructor(private modalService: MdbModalService) {}
-
-    @Input() public onSwitchHeatMap: () => void = () => {
-        console.log('Default switchHeatMap clicked!');
-    };
-
-    @Input() public onSwitchEmergencyAscent: () => void = () => {
-        console.log('Default switchEmergencyAscent clicked!');
-    };
-
-    @Input() public onSwitchFilter: () => void = () => {
-        console.log('Default switchFilter clicked!');
-    };
-
-    @Input() public onSharePlan: () => void = () => {
-        console.log('Default sharePlan clicked!');
-    };
-
-    @Input() public onTogglePricing: () => void = () => {
-        console.log('Default togglePricing clicked!');
-    };
 
     public openHelp(): void {
         this.modalRef = this.modalService.open(HelpModalComponent, {
--- projects/planner/src/app/help/help-modal.component.ts
+++ projects/planner/src/app/help/help-modal.component.ts
@@ -1,26 +1,38 @@
 import { Component, Input } from '@angular/core';
 import { MdbModalRef } from 'mdb-angular-ui-kit/modal';
 import { NgxMdModule } from 'ngx-md';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
+import { catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
 @Component({
     selector: 'app-help-modal',
     standalone: true,
-    imports: [ NgxMdModule ],
+    imports: [ NgxMdModule, HttpClientModule ],
     templateUrl: './help-modal.component.html',
     styleUrl: './help-modal.component.scss'
 })
 
 export class HelpModalComponent {
-    private _path = 'not-implemented';
+    private _path = 'assets/docs/not-implemented.md';
 
-    constructor(public modalRef: MdbModalRef<HelpModalComponent>) {}
+    constructor(public modalRef: MdbModalRef<HelpModalComponent>, private http: HttpClient) {}
 
     get path() {
         return this._path;
     }
 
     @Input()
-    set path(value) {
-        this._path = `assets/docs/${value}.md`;
+    set path(value: string) {
+        const filePath = `assets/docs/${value}.md`;
+        this.http.head(filePath, { observe: 'response' })
+            .pipe(
+                catchError(() => of(null))
+            )
+            .subscribe(response => {
+                if (response && response.status === 200) {
+                    this._path = filePath;
+                }
+            });
     }
 }

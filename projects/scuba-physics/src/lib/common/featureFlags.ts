35af4cfe2e3834da80f0668bb93db50d36115311
Darkvikis <48491740+Darkvikis@users.noreply.github.com>
2025-04-20 21:43:28 +0200

    Set integratedHelp feature flag to false

--- projects/scuba-physics/src/lib/common/featureFlags.ts
+++ projects/scuba-physics/src/lib/common/featureFlags.ts
@@ -3,7 +3,7 @@ export class FeatureFlags
 {
     private static _instance: FeatureFlags;
 
-    public integratedHelp = true;
+    public integratedHelp = false;
 
     private constructor() {
     }

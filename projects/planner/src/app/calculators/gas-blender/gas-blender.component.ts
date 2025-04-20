ca50cca2f7d7a80d705eca5067e60ea07f113711
Darkvikis <48491740+Darkvikis@users.noreply.github.com>
2025-04-20 22:09:31 +0200

    Remove console log from togglePricing method and update card header titles in depths-complex and depths-simple components for consistency

--- projects/planner/src/app/calculators/gas-blender/gas-blender.component.ts
+++ projects/planner/src/app/calculators/gas-blender/gas-blender.component.ts
@@ -141,7 +141,6 @@ export class GasBlenderComponent implements OnInit {
 
     public togglePricing(): void {
         this.showPricing = !this.showPricing;
-        console.log('Pricing toggled:', this.showPricing);
     }
 
     public applyTemplate(): void {

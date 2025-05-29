18b77ea695780c072f23c2b268926b7ba8ec1e9c
Jiri Pokorny <j.pok@post.cz>
2025-05-29 09:34:25 +0200

    Added volumes conversion to gas blender

--- projects/scuba-physics/src/lib/calculators/gasBlender.ts
+++ projects/scuba-physics/src/lib/calculators/gasBlender.ts
@@ -4,6 +4,7 @@ import { Compressibility } from '../physics/compressibility';
 import { StandardGases } from '../gases/StandardGases';
 import { Gas } from '../gases/Gases';
 import { GasMixtures } from '../gases/GasMixtures';
+import { GasMix } from "./realGasBlender";
 
 /**
  * Blending result showing amount of each component used
@@ -132,40 +133,81 @@ export class GasBlender {
         GasBlender.validate(request.target, 'Target');
         GasBlender.validate(request.topMix, 'Top');
 
+        const compressibility = new Compressibility();
+        const targetGas = new Gas(request.target.o2, request.target.he);
+        const targetVolume = compressibility.normalVolume(request.target.pressure, targetGas);
+        const initialGas = new Gas(request.source.o2, request.source.he);
+        const sourceVolume = compressibility.normalVolume(request.source.pressure, initialGas);
+
         const finalfN2 = GasBlender.fN2(request.target);
         const finalN2Bars  = finalfN2 * request.target.pressure;
+        const finalN2Volume = finalfN2 * targetVolume;
+
         const currentfN2 = GasBlender.fN2(request.source);
         const currentN2Bars = currentfN2 * request.source.pressure;
+        const currentN2Volume = currentfN2 * sourceVolume;
+
         let addN2Bars = finalN2Bars - currentN2Bars;
+        let addN2Volume = finalN2Volume - currentN2Volume;
         addN2Bars = Precision.round(addN2Bars, 8);
+        addN2Volume = Precision.round(addN2Volume, 8);
 
         // Even the top mix contains more nitrogen than target, we are still able to mix
         // by adding less top mix and more He and O2
         if(addN2Bars < 0) {
             const removeSourceBars = -(addN2Bars / currentfN2);
-            return GasBlender.mixByRemoving(request, removeSourceBars);
+            const removeSourceVolume = -(addN2Volume / currentfN2);
+            return GasBlender.mixByRemoving(request, removeSourceBars); // TODO
         }
 
         const topfN2 = GasBlender.fN2(request.topMix);
         const addTopBars = addN2Bars / topfN2;
-        const targetHeBars = GasBlender.pressureHe(request.target);
-        const sourceHeBars = GasBlender.pressureHe(request.source);
+        const addTopVolume = addN2Volume / topfN2;
+        const targetHeBars = request.target.he * request.target.pressure;
+        const targetHeVolume = request.target.he * targetVolume;
+        const sourceHeBars = request.source.he * request.source.pressure;
+        const sourceHeVolume = request.source.he * sourceVolume;
         const topHeBars = addTopBars * request.topMix.he;
+        const topHeVolume = addTopVolume * request.topMix.he;
         let addHeBars = targetHeBars - sourceHeBars - topHeBars;
+        let addHeVolume = targetHeVolume - sourceHeVolume - topHeVolume;
         addHeBars = Precision.round(addHeBars, 8);
+        addHeVolume = Precision.round(addHeVolume, 8);
 
         if(addHeBars < 0) {
             const removeSourceBars = -(addHeBars / request.source.he);
-            return GasBlender.mixByRemoving(request, removeSourceBars);
+            const removeSourceVolume = -(addHeVolume / request.source.he);
+            return GasBlender.mixByRemoving(request, removeSourceBars); // TODO
         }
 
         let addO2Bars = request.target.pressure - request.source.pressure - addHeBars - addTopBars;
+        let addO2Volume = targetVolume - sourceVolume - addHeVolume - addTopVolume;
         addO2Bars = Precision.round(addO2Bars, 8);
+        addO2Volume = Precision.round(addO2Volume, 8);
 
         if(addO2Bars < 0) {
             const removeSourceBars = -(addO2Bars / request.source.o2);
-            return GasBlender.mixByRemoving(request, removeSourceBars);
+            const removeSourceVolume = -(addO2Volume / request.source.o2);
+            return GasBlender.mixByRemoving(request, removeSourceBars); // TODO
         }
+
+        const volumeAfterHe = sourceVolume + addHeVolume;
+
+        // const mixAfterHe = new GasMix(100 * (gasi.fO2 * ivol + gas1.fO2 * top1) / (ivol + top1),
+        //     100 * (gasi.fHe * ivol + gas1.fHe * top1) / (ivol + top1));
+
+        // const pressureAfterHe = this.compress.pressure(mixAfterO2, ivol + top1);
+        // addHeBars = pressureAfterHe - request.source.pressure;
+
+        const volumeAfterO2 = sourceVolume + addHeVolume + addO2Volume;
+        // const mixAfterO2 = new GasMix(100 * (gasi.fO2 * ivol + gas1.fO2 * top1 + gas2.fO2 * top2) / (ivol + top1 + top2),
+        //     100 * (gasi.fHe * ivol + gas1.fHe * top1 + gas2.fHe * top2) / (ivol + top1 + top2));
+
+        // const pressureAfterO2 = this.compress.pressure(newmix2.toGas(), ivol + top1 + top2);
+        // addO2Bars = pressureAfterO2 - pressureAfterHe;
+
+        // mixAfterTop = targetGas;
+        // addTopBars = request.target.pressure - pressureAfterO2;
 
         return {
             addO2: addO2Bars,
@@ -189,6 +231,21 @@ export class GasBlender {
         result.removeFromSource += removeSourceBars;
         return result;
     }
+
+    // private static mixByRemovingVolume(removeSourceVolume: number) {
+    //     const newRequest = GasBlender.copyRequest(request);
+    //     // const expectedRemove = Precision.floor(removeSource, 8);
+    //
+    //     if(removeSourceVolume > request.source.pressure) {
+    //         throw new Error('Unable to mix required gas because target contains less he or oxygen than top mix.');
+    //     }
+    //
+    //     newRequest.source.pressure -= removeSourceVolume;
+    //     const result =  GasBlender.mix(newRequest);
+    //     // aggregate the removed pressure from all the recursive calls
+    //     result.removeFromSource += removeSourceVolume;
+    //     return result;
+    // }
 
     private static fN2(mix: Mix): number {
         return GasMixtures.n2(mix.o2, mix.he);
@@ -236,9 +293,5 @@ export class GasBlender {
             he: source.he,
             pressure: source.pressure
         };
-    }
-
-    private static pressureHe(tank: TankMix): number {
-        return tank.he * tank.pressure;
     }
 }
